3a4cd82b6aeff2bb0e2d1f153a428e321244418f
samreid <reids@colorado.edu>
2016-04-13 13:30:55 -0600

    Moved TandemDragHandler to tandem/ and added tandem to config, see https://github.com/phetsims/tandem/issues/9

--- js/projectile-motion-config.js
+++ js/projectile-motion-config.js
@@ -36,6 +36,7 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
+    TANDEM: '../../tandem/js',
 
     // this sim
     PROJECTILE_MOTION: '.'
