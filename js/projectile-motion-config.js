79f49543eed5582d59e3ef5cce28ba4a4a5b0cfa
samreid <reids@colorado.edu>
2016-05-23 12:26:31 -0600

    Added ifphetio plugin and PHET_IO path to config file, see https://github.com/phetsims/phet-io/issues/453

--- js/projectile-motion-config.js
+++ js/projectile-motion-config.js
@@ -23,6 +23,7 @@ require.config( {
     image: '../../chipper/js/requirejs-plugins/image',
     mipmap: '../../chipper/js/requirejs-plugins/mipmap',
     string: '../../chipper/js/requirejs-plugins/string',
+    ifphetio: '../../chipper/js/requirejs-plugins/ifphetio',
 
     // common directories, uppercase names to identify them in require imports
     AXON: '../../axon/js',
@@ -31,6 +32,7 @@ require.config( {
     JOIST: '../../joist/js',
     KITE: '../../kite/js',
     PHET_CORE: '../../phet-core/js',
+    PHET_IO: '../../phet-io/js',
     PHETCOMMON: '../../phetcommon/js',
     REPOSITORY: '..',
     SCENERY: '../../scenery/js',
