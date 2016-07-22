b58fbdb09e8477d85de8a196568651a8034075db
andrea-phet <andrealin2018@gmail.com>
2016-07-21 19:13:11 -0600

    fix merge problems in tracer node

--- js/common/view/TracerNode.js
+++ js/common/view/TracerNode.js
@@ -102,70 +102,7 @@ define( function( require ) {
         tracerModel.y = modelViewTransform.viewToModelY( startY + change.y );
       }
 
-<<<<<<< Updated upstream
     } ) );
-=======
-    // crosshair view
-    var crosshairShape = new Shape()
-      .moveTo( -CIRCLE_RADIUS, 0 )
-      .lineTo( CIRCLE_RADIUS, 0 )
-      .moveTo( 0, -CIRCLE_RADIUS )
-      .lineTo( 0, CIRCLE_RADIUS );
-
-    var crosshair = new Path( crosshairShape, { stroke: 'black' } );
-
-    var circle = new Circle( CIRCLE_RADIUS, { lineWidth: 2, stroke: 'black' } );
-
-    // information readouts
-    var timeText = new Text( 'Time (s): ', ProjectileMotionConstants.LABEL_TEXT_OPTIONS );
-    var rangeText = new Text( 'Range (m): ', ProjectileMotionConstants.LABEL_TEXT_OPTIONS );
-    var heightText = new Text( 'Height (m): ', ProjectileMotionConstants.LABEL_TEXT_OPTIONS );
-
-    var textBox = new VBox( {
-      align: 'left',
-      children: [
-        timeText,
-        rangeText,
-        heightText
-      ]
-    } );
-
-    // Listen for when time, range, and height change, and update the readouts.
-    Property.multilink( [ tracerModel.timeProperty, tracerModel.rangeProperty, tracerModel.heightProperty ], function(
-      time,
-      range,
-      height
-    ) {
-      timeText.text = 'Time (s): ' + ( time ? time.toFixed( 2 ) : '' );
-      rangeText.text = 'Range (m): ' + ( range ? range.toFixed( 2 ) : '' );
-      heightText.text = 'Height (m): ' + ( height ? height.toFixed( 2 ) : '' );
-    } );
-
-    // Listen for location changes, align locations, and update model.
-    Property.multilink( [ tracerModel.xProperty, tracerModel.yProperty ], function() {
-      thisNode.probeOrigin.x = modelViewTransform.modelToViewX( tracerModel.x );
-      thisNode.probeOrigin.y = modelViewTransform.modelToViewY( tracerModel.y );
-
-      crosshair.center = thisNode.probeOrigin;
-      circle.center = thisNode.probeOrigin;
-      rectangle.centerX = thisNode.probeOrigin.x;
-      rectangle.top = thisNode.probeOrigin.y + CIRCLE_RADIUS;
-      textBox.left = rectangle.left + 5;
-      textBox.top = rectangle.top + 5;
-
-      thisNode.tracerModel.updateData(); // TODO: investiage, this may be create a cycle
-    } );
-
-    // Rendering order
-    Node.call( thisNode, {
-      children: [
-        rectangle,
-        crosshair,
-        circle,
-        textBox
-      ]
-    } );
->>>>>>> Stashed changes
 
   }
 
