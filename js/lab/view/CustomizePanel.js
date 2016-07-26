0df0c1122648e97104afb5024b6de9a2f366034f
andrea-phet <andrealin2018@gmail.com>
2016-07-26 16:59:53 -0600

    refactor: make parameterBox an object with related properties, pull on focus functions, add focus rectangle as a cue, detach air resistance in customize form from model property

--- js/lab/view/CustomizePanel.js
+++ js/lab/view/CustomizePanel.js
@@ -25,6 +25,7 @@ define( function( require ) {
   var Node = require( 'SCENERY/nodes/Node' );
   var Property = require( 'AXON/Property' );
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
+  var Path = require( 'SCENERY/nodes/Path' );
 
   // strings
   var heightString = 'Height';
@@ -64,13 +65,13 @@ define( function( require ) {
       options
     );
 
-    // @private {array.<{valueText, property}>} contains the text of parameters and associated properties
-    this.parameters = [];
-    // TODO: this.currentParameter
+    // @private {array.<{Object}>} parameters contains objects like currentParameter
+    this.parameters;
+    this.currentParameter; // {Object} contains properties about a parameter box
 
     this.numberKeypadStringProperty = new Property( '' );
 
-    // create hboxes for the parameter label, readout, and pencil button
+    // create hboxes for the parameter label, readout, and edit button
     var heightBox = this.createParameterControlBox(
       heightString,
       projectileMotionLabModel.cannonHeightProperty,
@@ -113,22 +114,34 @@ define( function( require ) {
       ProjectileMotionConstants.ALTITUDE_RANGE
     );
 
+    this.parameters = [
+      heightBox,
+      angleBox,
+      velocityBox,
+      massBox,
+      diameterBox,
+      dragCoefficientBox,
+      altitudeBox
+    ];
+
     var airResistanceLabel = new Text( airResistanceString, LABEL_OPTIONS );
-    var airResistanceCheckBox = new CheckBox( airResistanceLabel, projectileMotionLabModel.airResistanceOnProperty );
+    this.modelAirResistanceProperty = projectileMotionLabModel.airResistanceOnProperty;
+    this.detachedAirResistanceProperty = new Property( this.modelAirResistanceProperty.get() );
+    var airResistanceCheckBox = new CheckBox( airResistanceLabel, this.detachedAirResistanceProperty );
 
     // The contents of the control panel
     var leftSideContent = new VBox( {
       align: 'right',
       spacing: 10,
       children: [
-        heightBox,
-        angleBox,
-        velocityBox,
-        massBox,
-        diameterBox,
+        heightBox.node,
+        angleBox.node,
+        velocityBox.node,
+        massBox.node,
+        diameterBox.node,
         airResistanceCheckBox,
-        dragCoefficientBox,
-        altitudeBox
+        dragCoefficientBox.node,
+        altitudeBox.node
       ]
     } );
 
@@ -180,40 +193,73 @@ define( function( require ) {
       this.parameters.forEach( function( parameter ) {
         parameter.valueText.setText( parameter.property.get().toFixed( 2 ) );
       } );
+      this.detachedAirResistanceProperty.set( this.modelAirResistanceProperty.get() );
       this.visible = true;
     },
 
     // not sure @public or @private, closes self and sets values
     closeSelf: function() {
-      this.numberKeypad.digitStringProperty.unlinkAll();
+      this.unfocusCurrent();
 
       // set the properties with values in the text readouts
       this.parameters.forEach( function( parameter ) {
         parameter.property.set( Number( parameter.valueText.getText() ) );
       } );
+      this.modelAirResistanceProperty.set( this.detachedAirResistanceProperty.get() );
       this.visible = false;
     },
 
+    // @private, sets focus on this parameter box
+    // @param {Object} parameterBox
+    setFocus: function( parameterBox ) {
+      this.currentParameter = parameterBox;
+      // TODO: right after clicking pencil button, clicking backspace does not clear. It should.
+      var linkListener = this.currentParameter.keypadListener;
+      this.numberKeypad.armForNewEntry();
+      this.numberKeypad.digitStringProperty.lazyLink( linkListener );
+      this.currentParameter.focusRectangle.visible = true;
+    },
+
+    // @private, unlinks property and unhighlights current parameter box
+    unfocusCurrent: function() {
+      // no parameter box has been focused on
+      if ( !this.currentParameter ) {
+        return;
+      }
+      var linkListener = this.currentParameter.keypadListener;
+      this.numberKeypad.digitStringProperty.unlink( linkListener );
+      this.currentParameter.focusRectangle.visible = false;
+      // TODO: if no entry for this parameter on unfocus, or submit, set to default, or what it was before
+    },
+
+    // @private
+    editButtonListener: function( parameterBox ) {
+      this.unfocusCurrent();
+      this.setFocus( parameterBox );
+    },
+
     /**
-     * Auxiliary function that creates hbox for a parameter label, text readout, and pencil button
+     * Auxiliary function that creates hbox for a parameter label, text readout, and edit button
      * Also adds value text and associated property to the running list of parameters, {array.{Object}} parameters
      * @param {string} label
      * @param {Property.<number>} property - the property that is used and set
      * @param {Object} range, range has keys min and max
-     * @returns {VBox}
+     * @returns {Object}: {HBox} node, {Text} valueText, {Rectangle} focusRectangle, {Property} property, {function} keypadListener
      * @private
      */
     createParameterControlBox: function( label, property, range ) {
       var self = this;
+
+      // this will be returned
+      var parameterBox = new Object();
+
+      // label
       var parameterLabel = new Text( label, LABEL_OPTIONS );
 
-      // value
+      // value text
       var valueText = new Text( property.get().toFixed( 2 ), LABEL_OPTIONS );
 
-      // add text and property to running list
-      this.parameters.push( { valueText: valueText, property: property } );
-
-      // @private background for text
+      // background for text
       var backgroundNode = new Rectangle(
         0, // x
         0, // y
@@ -223,35 +269,37 @@ define( function( require ) {
         4, // cornerYRadius
         TEXT_BACKGROUND_OPTIONS
       );
-
       valueText.centerY = backgroundNode.centerY;
       valueText.left = backgroundNode.left + TEXT_MARGIN;
 
-      var valueNode = new Node( { children: [ backgroundNode, valueText ] } );
+      // visible if text field is focused on
+      var focusRectangle = new Path( backgroundNode.getShape(), {
+        visible: false,
+        stroke: 'black',
+        lineWidth: 3
+      } );
 
-      var linkTextToKeypad = function( numberKeypadString ) {
+      var valueNode = new Node( { children: [ backgroundNode, focusRectangle, valueText ] } );
+
+      // edit button
+      var pencilIcon = new FontAwesomeNode( 'pencil_square_o', { scale: 0.35 } );
+      var editButton = new RectangularPushButton( {
+        minWidth: 25,
+        minHeight: 20,
+        content: pencilIcon,
+        listener: self.editButtonListener.bind( self, parameterBox )
+      } );
+
+      // properties in one place
+      parameterBox.node = new HBox( { spacing: 10, children: [ parameterLabel, valueNode, editButton ] } );
+      parameterBox.valueText = valueText;
+      parameterBox.focusRectangle = focusRectangle;
+      parameterBox.property = property;
+      parameterBox.keypadListener = function( numberKeypadString ) {
         valueText.setText( numberKeypadString );
       };
 
-      var pencilIcon = new FontAwesomeNode( 'pencil_square_o', { scale: 0.35 } );
-
-      var pencilButtonListener = function() {
-        // This property doesn't have any listeners except defined in this function, so unlink all is safe.
-        // TODO: figure out how to keep reference to previously linked
-        self.numberKeypad.digitStringProperty.unlinkAll();
-        self.numberKeypad.armForNewEntry();
-        self.numberKeypad.digitStringProperty.lazyLink( linkTextToKeypad );
-        // TODO: if no entry for this parameter on submit, set to default, or what it was before
-      };
-
-      var pencilButton = new RectangularPushButton( {
-        minWidth: 25,
-        minHeight: 20,
-        listener: pencilButtonListener,
-        content: pencilIcon
-      } );
-
-      return new HBox( { spacing: 10, children: [ parameterLabel, valueNode, pencilButton ] } );
+      return parameterBox;
     }
 
   } );
