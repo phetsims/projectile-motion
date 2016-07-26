a55a25f3f097129b3727cea921b616cfba3aefd4
andrea-phet <andrealin2018@gmail.com>
2016-07-26 10:44:31 -0600

    add left side parameter readouts and pencil buttons

--- js/lab/view/CustomizePanel.js
+++ js/lab/view/CustomizePanel.js
@@ -9,10 +9,8 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Dimension2 = require( 'DOT/Dimension2' );
-  // var CheckBox = require( 'SUN/CheckBox' );
+  var CheckBox = require( 'SUN/CheckBox' );
   var HBox = require( 'SCENERY/nodes/HBox' );
-  var HSlider = require( 'SUN/HSlider' );
   // var HStrut = require( 'SCENERY/nodes/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
   var NumberKeypad = require( 'SCENERY_PHET/NumberKeypad' );
@@ -24,12 +22,14 @@ define( function( require ) {
   var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
 
   // strings
-  // var massString = 'Mass';
-  // var diameterString = 'Diameter';
-  // var dragCoefficientString = 'Drag Coefficient';
-  // var altitudeString = 'Altitude';
-  // var airResistanceString = 'Air Resistance';
-  // var velocityVectorsString = 'Velocity Vectors';
+  var heightString = 'Height';
+  var angleString = require( 'string!PROJECTILE_MOTION/angle' );
+  var velocityString = require( 'string!PROJECTILE_MOTION/velocity' );
+  var massString = 'Mass';
+  var diameterString = 'Diameter';
+  var dragCoefficientString = 'Drag Coefficient';
+  var altitudeString = 'Altitude';
+  var airResistanceString = 'Air Resistance';
 
   // constants
   var LABEL_OPTIONS = ProjectileMotionConstants.PANEL_LABEL_OPTIONS;
@@ -50,6 +50,67 @@ define( function( require ) {
       },
       options );
 
+    var heightBox = this.createParameterControlBox(
+      heightString,
+      projectileMotionLabModel.cannonHeightProperty,
+      ProjectileMotionConstants.CANNON_HEIGHT_RANGE
+    );
+
+    var angleBox = this.createParameterControlBox(
+      angleString,
+      projectileMotionLabModel.cannonAngleProperty,
+      ProjectileMotionConstants.CANNON_ANGLE_RANGE
+    );
+
+    var velocityBox = this.createParameterControlBox(
+      velocityString,
+      projectileMotionLabModel.launchVelocityProperty,
+      ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE
+    );
+
+    var massBox = this.createParameterControlBox(
+      massString,
+      projectileMotionLabModel.projectileMassProperty,
+      ProjectileMotionConstants.PROJECTILE_MASS_RANGE
+    );
+
+    var diameterBox = this.createParameterControlBox(
+      diameterString,
+      projectileMotionLabModel.projectileDiameterProperty,
+      ProjectileMotionConstants.PROJECTILE_DIAMETER_RANGE
+    );
+
+    var dragCoefficientBox = this.createParameterControlBox(
+      dragCoefficientString,
+      projectileMotionLabModel.projectileDragCoefficientProperty,
+      ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE
+    );
+
+    var altitudeBox = this.createParameterControlBox(
+      altitudeString,
+      projectileMotionLabModel.altitudeProperty,
+      ProjectileMotionConstants.ALTITUDE_RANGE
+    );
+
+    var airResistanceLabel = new Text( airResistanceString, LABEL_OPTIONS );
+    var airResistanceCheckBox = new CheckBox( airResistanceLabel, projectileMotionLabModel.airResistanceOnProperty );
+
+    // The contents of the control panel
+    var leftSideReadouts = new VBox( {
+      align: 'left',
+      spacing: 10,
+      children: [
+        heightBox,
+        angleBox,
+        velocityBox,
+        massBox,
+        diameterBox,
+        airResistanceCheckBox,
+        dragCoefficientBox,
+        altitudeBox
+      ]
+    } );
+
     var numberKeypad = new NumberKeypad( {
       decimalPointKey: true
     } );
@@ -57,8 +118,9 @@ define( function( require ) {
     // The contents of the control panel
     var content = new HBox( {
       align: 'center',
-      spacing: 5,
+      spacing: 15,
       children: [
+        leftSideReadouts,
         numberKeypad
       ]
     } );
@@ -95,17 +157,12 @@ define( function( require ) {
       property.link( function( v ) {
         parameterLabel.text = thisPanel.createLabelText( label, v );
       } );
-      var setParameterSlider = new HSlider( property, range, {
-        maxHeight: 30,
-        trackSize: new Dimension2( 150, 6 )
-      } );
       var pencilButton = new RectangularPushButton( {
         minWidth: 25,
         minHeight: 20,
         listener: function() { console.log( 'pencil button pressed' ); }
       } );
-      var leftBox = new VBox( { spacing: 2, children: [ parameterLabel, setParameterSlider ] } );
-      return new HBox( { spacing: 10, children: [ leftBox, pencilButton ] } );
+      return new HBox( { spacing: 10, children: [ parameterLabel, pencilButton ] } );
     }
 
   } );
