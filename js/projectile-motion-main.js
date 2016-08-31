ca9e9390e5fd81af9f40d531951dbc1ef1b8e5fd
Chris Malley <cmalley@pixelzoom.com>
2016-08-31 14:46:56 -0600

    delete no-op simOptions extend from main.js, https://github.com/phetsims/simula-rasa/issues/8

--- js/projectile-motion-main.js
+++ js/projectile-motion-main.js
@@ -30,13 +30,6 @@ define( function( require ) {
     }
   };
 
-  // Appending '?dev' to the URL will enable developer-only features.
-  if ( phet.chipper.getQueryParameter( 'dev' ) ) {
-    simOptions = _.extend( {
-      // add dev-specific options here
-    }, simOptions );
-  }
-
   SimLauncher.launch( function() {
     var sim = new Sim( projectileMotionTitleString, [
       new ProjectileMotionIntroScreen(),
