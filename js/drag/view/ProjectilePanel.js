0d0a6dff24ab96c8505fa81c00cf9c8efbcbece3
Andrea Lin <andrealin2018@gmail.com>
2017-03-18 14:48:12 -0600

    Change parameter controls on drag screen

--- js/drag/view/ProjectilePanel.js
+++ js/drag/view/ProjectilePanel.js
@@ -11,7 +11,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var CheckBox = require( 'SUN/CheckBox' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var HBox = require( 'SCENERY/nodes/HBox' );
   var HSlider = require( 'SUN/HSlider' );
@@ -28,20 +27,20 @@ define( function( require ) {
   var VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var pattern0Label1UnitsString = require( 'string!PROJECTILE_MOTION/pattern0Label1Units' );
+  var pattern0Value1UnitsWithSpaceString = require( 'string!PROJECTILE_MOTION/pattern0Value1UnitsWithSpace' );
+  var altitudeString = require( 'string!PROJECTILE_MOTION/altitude' );
   var massString = require( 'string!PROJECTILE_MOTION/mass' );
   var kgString = require( 'string!PROJECTILE_MOTION/kg' );
   var diameterString = require( 'string!PROJECTILE_MOTION/diameter' );
   var mString = require( 'string!PROJECTILE_MOTION/m' );
   var dragCoefficientString = require( 'string!PROJECTILE_MOTION/dragCoefficient' );
-  var airResistanceString = require( 'string!PROJECTILE_MOTION/airResistance' );
 
   // constants
   var LABEL_OPTIONS = ProjectileMotionConstants.PANEL_LABEL_OPTIONS;
-  var BIGGER_LABEL_OPTIONS = ProjectileMotionConstants.PANEL_BIGGER_LABEL_OPTIONS;
   var TEXT_BACKGROUND_OPTIONS = {
     fill: 'white',
-    stroke: 'black'
+    stroke: 'lightGray',
+    strokeWidth: 1
   };
   var DRAG_OBJECT_DISPLAY_RADIUS = 12;
 
@@ -69,22 +68,20 @@ define( function( require ) {
      */
     function createParameterControlBox( labelString, unitsString, property, range, viewNode ) {
       // label
-      var parameterLabel = new Text( unitsString ? StringUtils.format( pattern0Label1UnitsString, labelString, unitsString ) : labelString,
-        LABEL_OPTIONS
-      );
+      var parameterLabel = new Text( labelString, LABEL_OPTIONS );
 
       // value text
-      var valueText = new Text( property.get().toFixed( 2 ), viewNode ? LABEL_OPTIONS: _.defaults( { fill: 'blue' }, LABEL_OPTIONS ) );
+      var valueText = new Text( unitsString ? StringUtils.format( pattern0Value1UnitsWithSpaceString, property.get().toFixed( 2 ), unitsString ) : property.get().toFixed( 2 ), LABEL_OPTIONS );
 
       // background for text
       var backgroundNode = new Rectangle(
         0, // x
         0, // y
-        options.textDisplayWidth, // width
+        options.textDisplayWidth * 1.5, // width, widened
         valueText.height + 2 * options.textDisplayYMargin, // height
-        4, // cornerXRadius
-        4, // cornerYRadius
-        viewNode ? TEXT_BACKGROUND_OPTIONS : _.defaults( { fill: ProjectileMotionConstants.LIGHT_GRAY }, TEXT_BACKGROUND_OPTIONS )
+        1, // cornerXRadius
+        1, // cornerYRadius
+        TEXT_BACKGROUND_OPTIONS
       );
 
       // text node updates if property value changes
@@ -93,6 +90,17 @@ define( function( require ) {
         valueText.center = backgroundNode.center;
       } );
 
+      var slider = new HSlider( property, range, {
+        constrainValue: function( value ) { return Math.round( value * 100 ) / 100; }, // two decimal place accuracy
+        majorTickLength: 5,
+        trackSize: new Dimension2( options.minWidth - 2 * options.xMargin - 20, 0.5 ),
+        thumbSize: new Dimension2( 16, 28 ),
+        thumbTouchAreaXDilation: 6,
+        thumbTouchAreaYDilation: 4 // smaller to prevent overlap with above number spinner buttons
+      } );
+      slider.addMajorTick( range.min );
+      slider.addMajorTick( range.max );
+
       var valueNode = new Node( { children: [ backgroundNode, valueText ] } );
 
       if ( viewNode ) {
@@ -100,28 +108,20 @@ define( function( require ) {
         var strut = new HStrut( 200 ); // empirically determined. Accounts for horizontal changes in viewNode
         var valueAndDisplay = new VBox( { align: 'right', children: [ strut, viewAndValueNodes ] } );
         var xSpacing = options.minWidth - 2 * options.xMargin - parameterLabel.width - valueAndDisplay.width;
-        return new HBox( { spacing: xSpacing, children: [ parameterLabel, valueAndDisplay ] } );
+        return new VBox( { spacing: options.yMargin, children: [
+            new HBox( { spacing: xSpacing, children: [ parameterLabel, valueAndDisplay ] } ),
+            slider
+        ] } );
       }
 
       else {
         xSpacing = options.minWidth - 2 * options.xMargin - parameterLabel.width - valueNode.width;
-        return new HBox( { spacing: xSpacing, children: [ parameterLabel, valueNode ] } );
+        return new VBox( { spacing: options.yMargin, children: [
+            new HBox( { spacing: xSpacing, children: [ parameterLabel, valueNode ] } ),
+            slider
+        ] } );
       }
     }
-
-    var massBox = createParameterControlBox(
-      massString,
-      kgString,
-      projectileMotionLabModel.projectileMassProperty,
-      ProjectileMotionConstants.PROJECTILE_MASS_RANGE
-    );
-
-    var diameterBox = createParameterControlBox(
-      diameterString,
-      mString,
-      projectileMotionLabModel.projectileDiameterProperty,
-      ProjectileMotionConstants.PROJECTILE_DIAMETER_RANGE
-    );
 
     var dragObjectDisplay = new Node();
     dragObjectDisplay.addChild( new HStrut( DRAG_OBJECT_DISPLAY_RADIUS * 2 ) );
@@ -143,44 +143,40 @@ define( function( require ) {
       dragObjectDisplay.addChild( objectView );
     });
 
-    var dragSlider = new HSlider(
-      projectileMotionLabModel.projectileDragCoefficientProperty,
-      ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE, {
-      constrainValue: function( value ) { return Math.round( value * 100 ) / 100; }, // two decimal place accuracy
-      majorTickLength: 5,
-      trackSize: new Dimension2( options.minWidth - 2 * options.xMargin - 20, 0.5 ),
-      thumbSize: new Dimension2( 16, 28 ),
-      thumbTouchAreaXDilation: 6,
-      thumbTouchAreaYDilation: 4 // smaller to prevent overlap with above number spinner buttons
-    } );
-    dragSlider.addMajorTick( ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE.min );
-    dragSlider.addMajorTick( ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE.max );
+    var diameterBox = createParameterControlBox(
+      diameterString,
+      mString,
+      projectileMotionLabModel.projectileDiameterProperty,
+      ProjectileMotionConstants.PROJECTILE_DIAMETER_RANGE
+    );
 
-    var airResistanceLabel = new Text( airResistanceString, BIGGER_LABEL_OPTIONS );
-    var airResistanceCheckBox = new CheckBox( airResistanceLabel, projectileMotionLabModel.airResistanceOnProperty );
+    var massBox = createParameterControlBox(
+      massString,
+      kgString,
+      projectileMotionLabModel.projectileMassProperty,
+      ProjectileMotionConstants.PROJECTILE_MASS_RANGE
+    );
+
+    var altitudeBox = createParameterControlBox(
+      altitudeString,
+      mString,
+      projectileMotionLabModel.altitudeProperty,
+      ProjectileMotionConstants.ALTITUDE_RANGE
+    );
 
     // The contents of the control panel
     var content = new VBox( {
       align: 'left',
       spacing: options.controlsVerticalSpace,
       children: [
-        massBox,
+        dragCoefficientBox,
         diameterBox,
-        airResistanceCheckBox,
-        dragCoefficientBox
+        massBox,
+        altitudeBox
       ]
     } );
 
-    var initialValuesVBox = new VBox( {
-      align: 'center',
-      spacing: options.controlsVerticalSpace,
-      children: [
-        content,
-        dragSlider
-      ]
-    } );
-
-    Panel.call( this, initialValuesVBox, options );
+    Panel.call( this, content, options );
   }
 
   projectileMotion.register( 'ProjectilePanel', ProjectilePanel );
