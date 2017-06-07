6af89d48577e88d1db809085597d72871b86f5a0
Andrea Lin <andrealin2018@gmail.com>
2017-06-07 14:13:30 -0600

    Use ArrowNode for height leader line

--- js/common/view/CannonNode.js
+++ js/common/view/CannonNode.js
@@ -10,17 +10,16 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
   var Circle = require( 'SCENERY/nodes/Circle' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
   var projectileMotion = require( 'PROJECTILE_MOTION/projectileMotion' );
   var ProjectileMotionConstants = require( 'PROJECTILE_MOTION/common/ProjectileMotionConstants' );
-  var Path = require( 'SCENERY/nodes/Path' );
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var Vector2 = require( 'DOT/Vector2' );
   var Util = require( 'DOT/Util' );
-  var Shape = require( 'KITE/Shape' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
 
@@ -69,49 +68,40 @@ define( function( require ) {
     this.addChild( cannon );
 
     // add line for indicating the height
-    var heightLeaderLine = new Line(
+    var heightLeaderLine = new ArrowNode(
       modelViewTransform.modelToViewX( HEIGHT_LEADER_LINE_POSITION ),
       modelViewTransform.modelToViewY( 0 ),
       modelViewTransform.modelToViewX( HEIGHT_LEADER_LINE_POSITION ),
       modelViewTransform.modelToViewY( heightProperty.get() ), {
         stroke: 'black',
+        headHeight: 5,
+        headWidth: 5,
         lineDash: [ 5, 5 ],
-        lineWidth: 1
+        tailWidth: 0,
+        doubleHead: true
       }
     );
     this.addChild( heightLeaderLine );
 
     // draw the line caps for the height leader line
-    
-    var topCapShape = new Shape();
-    topCapShape.moveTo( 0, 0 );
-    topCapShape.lineTo( 0, 5 );
-    topCapShape.moveTo( -6, 0 );
-    topCapShape.lineTo( 6, 0 );
 
-    var heightLeaderLineTopCap = new Path( topCapShape, {
+    var heightLeaderLineTopCap = new Line( -6, 0, 6, 0, {
         stroke: 'black',
         lineWidth: 2
     } );
     this.addChild( heightLeaderLineTopCap );
-
-    var bottomCapShape = new Shape();
-    bottomCapShape.moveTo( 0, 0 );
-    bottomCapShape.lineTo( 0, -5 );
-    bottomCapShape.moveTo( -6, 0 );
-    bottomCapShape.lineTo( 6, 0 );
     
-    var heightLeaderLineBottomCap = new Path( bottomCapShape, {
+    var heightLeaderLineBottomCap = new Line( -6, 0, 6, 0, {
         stroke: 'black',
         lineWidth: 2
     } );
-    heightLeaderLineBottomCap.x = heightLeaderLine.x1;
-    heightLeaderLineBottomCap.y = heightLeaderLine.y1;
+    heightLeaderLineBottomCap.x = heightLeaderLine.tipX;
+    heightLeaderLineBottomCap.y = heightLeaderLine.tipY;
     this.addChild( heightLeaderLineBottomCap );
 
     // height readout
     var heightLabel = new Text( StringUtils.format( pattern0Value1UnitsWithSpaceString, Util.toFixedNumber( heightProperty.get(), 2 ), mString ), LABEL_OPTIONS );
-    heightLabel.centerX = heightLeaderLine.x1;
+    heightLabel.centerX = heightLeaderLine.tipX;
     this.addChild( heightLabel );
 
     // add invisible node for dragging height
@@ -146,11 +136,11 @@ define( function( require ) {
     heightProperty.link( function( height ) {
       cannon.y1 = modelViewTransform.modelToViewY( height );
       cannon.y2 = getY2();
-      heightLeaderLine.y2 = cannon.y1;
-      heightLeaderLineTopCap.x = heightLeaderLine.x2;
-      heightLeaderLineTopCap.y = heightLeaderLine.y2;
+      heightLeaderLine.setTailAndTip( heightLeaderLine.tailX, heightLeaderLine.tailY, heightLeaderLine.tipX, cannon.y1 );
+      heightLeaderLineTopCap.x = heightLeaderLine.tipX;
+      heightLeaderLineTopCap.y = heightLeaderLine.tipY;
       heightLabel.text = StringUtils.format( pattern0Value1UnitsWithSpaceString, Util.toFixedNumber( height, 2 ), mString );
-      heightLabel.y = heightLeaderLine.y2 - 5;
+      heightLabel.y = heightLeaderLine.tipY - 5;
       adjustableHeightArea.y = cannon.y1;
       rotatableArea.y = cannon.y2;
     } );
