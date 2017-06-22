2d1e03917ed95c7331e05de9cc4bcf1598c38fe3
Andrea Lin <andrealin2018@gmail.com>
2017-06-22 14:29:05 -0600

    Lab initial values panel

--- js/lab/view/InitialValuesPanel.js
+++ js/lab/view/InitialValuesPanel.js
@@ -9,16 +9,15 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var HSlider = require( 'SUN/HSlider' );
+  // var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var NumberSpinner = require( 'SUN/NumberSpinner' );
+  // var NumberSpinner = require( 'SUN/NumberSpinner' );
   var Panel = require( 'SUN/Panel' );
   var projectileMotion = require( 'PROJECTILE_MOTION/projectileMotion' );
   var ProjectileMotionConstants = require( 'PROJECTILE_MOTION/common/ProjectileMotionConstants' );
-  var Property = require( 'AXON/Property' );
-  var Range = require( 'DOT/Range' );
+  // var Property = require( 'AXON/Property' );
+  // var Range = require( 'DOT/Range' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
   var VBox = require( 'SCENERY/nodes/VBox' );
@@ -26,7 +25,7 @@ define( function( require ) {
 
   // strings
   var initialValuesString = require( 'string!PROJECTILE_MOTION/initialValues' );
-  var pattern0Label1UnitsString = require( 'string!PROJECTILE_MOTION/pattern0Label1Units' );
+  var pattern0Value1UnitsWithSpaceString = require( 'string!PROJECTILE_MOTION/pattern0Value1UnitsWithSpace' );
   var pattern0Value1UnitsString = require( 'string!PROJECTILE_MOTION/pattern0Value1Units' );
   var heightString = require( 'string!PROJECTILE_MOTION/height' );
   var mString = require( 'string!PROJECTILE_MOTION/m' );
@@ -55,19 +54,19 @@ define( function( require ) {
     // The fourth object is options given at time of construction, which overrides all the others
     options = _.extend( {}, ProjectileMotionConstants.RIGHTSIDE_PANEL_OPTIONS, {}, options );
 
-    // find max label width
-    var exampleParameterSpinner = new NumberSpinner( new Property( 0 ), new Property( new Range( 0, 1 ) ), _.extend( {
-      arrowsPosition: 'leftRight',
-      xMargin: 8,
-      yMargin: options.textDisplayYMargin,
-      backgroundMinWidth: options.textDisplayWidth
-    }, LABEL_OPTIONS ) );
-    var maxLabelWidth = options.minWidth - 2 * options.xMargin - exampleParameterSpinner.width - options.xSpacing;
+    // // find max label width for internationalization
+    // var exampleParameterSpinner = new NumberSpinner( new Property( 0 ), new Property( new Range( 0, 1 ) ), _.extend( {
+    //   arrowsPosition: 'leftRight',
+    //   xMargin: 8,
+    //   yMargin: options.textDisplayYMargin,
+    //   backgroundMinWidth: options.textDisplayWidth
+    // }, LABEL_OPTIONS ) );
+    // var maxLabelWidth = options.minWidth - 2 * options.xMargin - exampleParameterSpinner.width - options.xSpacing;
 
-    var labelScale = Math.min( 1, maxLabelWidth / Math.max( new Text( StringUtils.format( pattern0Label1UnitsString, heightString, mString ), LABEL_OPTIONS ).width,
-      new Text( angleString, LABEL_OPTIONS ).width,
-      new Text( StringUtils.format( pattern0Label1UnitsString, speedString, metersPerSecondString ), LABEL_OPTIONS ).width
-    ) );
+    // var labelScale = Math.min( 1, maxLabelWidth / Math.max( new Text( StringUtils.format( pattern0Label1UnitsString, heightString, mString ), LABEL_OPTIONS ).width,
+    //   new Text( angleString, LABEL_OPTIONS ).width,
+    //   new Text( StringUtils.format( pattern0Label1UnitsString, speedString, metersPerSecondString ), LABEL_OPTIONS ).width
+    // ) );
 
     /**
      * Auxiliary function that creates vbox for a parameter label and slider
@@ -75,38 +74,19 @@ define( function( require ) {
      * @param {string} unitsString - units
      * @param {Property.<number>} property - the property that is set and linked to
      * @param {Range} range - range for the property value
+     * @param {string} degreeString - optional, just for the angle
      * @returns {VBox}
      * @private
      */
-    function createParameterControlBox( labelString, unitsString, property, range, valueUnitsString ) {
-      var parameterLabel = new Text( unitsString ? StringUtils.format( pattern0Label1UnitsString, labelString, unitsString ) : labelString,
-        LABEL_OPTIONS
-      );
+    function createParameterControlBox( labelString, unitsString, property, range, degreeString ) {
+      var parameterLabel = new Text( '', LABEL_OPTIONS );
 
-      var setParameterSpinner = new NumberSpinner( property, new Property( range ), _.extend( {
-        arrowsPosition: 'leftRight',
-        xMargin: 8,
-        yMargin: options.textDisplayYMargin,
-        backgroundMinWidth: options.textDisplayWidth,
-        backgroundMaxWidth: options.textDisplayWidth,
-        valuePattern: valueUnitsString ? StringUtils.format( pattern0Value1UnitsString, '{0}', valueUnitsString ) : '{0}'
-      }, LABEL_OPTIONS ) );
-
-      // TODO: layout for internationalized angle pattern string? E.g. backgroundMaxWidth?
-
-      parameterLabel.scale( labelScale );
-
-      var xSpacing = options.minWidth - 2 * options.xMargin - parameterLabel.width - setParameterSpinner.width;
-
-      var parameterBox = new HBox( {
-        align: 'center',
-        spacing: xSpacing,
-        children: [ parameterLabel, setParameterSpinner ]
+      property.link( function( value ) {
+        var valueReadout = degreeString ? StringUtils.format( pattern0Value1UnitsString, Util.toFixedNumber( value, 2 ), degreeString ) : StringUtils.format( pattern0Value1UnitsWithSpaceString, Util.toFixedNumber( value, 2 ), unitsString );
+        parameterLabel.setText( labelString + ': ' + valueReadout );
       } );
 
-      parameterBox.parameterLabel = parameterLabel; // attach as property to resize
-
-      return parameterBox;
+      return new VBox( { align: 'left', children: [ parameterLabel, new HStrut( options.minWidth - 2 * options.xMargin ) ] } );
     }
 
     var heightBox = createParameterControlBox(
@@ -131,17 +111,6 @@ define( function( require ) {
       ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE
     );
 
-    var velocitySlider = new HSlider( launchVelocityProperty, ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE, {
-      constrainValue: function( value ) { return Util.roundSymmetric( value ); },
-      majorTickLength: 5,
-      trackSize: new Dimension2( options.minWidth - 2 * options.xMargin - 20, 0.5 ),
-      thumbSize: new Dimension2( 16, 28 ),
-      thumbTouchAreaXDilation: 6,
-      thumbTouchAreaYDilation: 4 // smaller to prevent overlap with above number spinner buttons
-    } );
-    velocitySlider.addMajorTick( ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE.min );
-    velocitySlider.addMajorTick( ProjectileMotionConstants.LAUNCH_VELOCITY_RANGE.max );
-
     // contents of the panel
     var content = new VBox( {
       align: 'left',
@@ -156,18 +125,18 @@ define( function( require ) {
     var initialValuesTitle = new Text( initialValuesString, TITLE_OPTIONS );
     initialValuesTitle.maxWidth = options.minWidth - 2 * options.xMargin;
 
-
     var initialValuesVBox = new VBox( {
       align: 'center',
       spacing: options.controlsVerticalSpace,
       children: [
         initialValuesTitle,
         content,
-        velocitySlider
       ]
     } );
 
     Panel.call( this, initialValuesVBox, options );
+
+    // content.left = this.left;
   }
 
   projectileMotion.register( 'InitialValuesPanel', InitialValuesPanel );
