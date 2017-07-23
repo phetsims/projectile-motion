81fdec06cc67dbe1188bc792f381e613f4e209ae
Chris Malley <cmalley@pixelzoom.com>
2017-07-22 21:52:41 -0600

    fix doc typo #103

--- js/common/model/DataPoint.js
+++ js/common/model/DataPoint.js
@@ -33,7 +33,7 @@ define( function( require ) {
     assert && assert( !isNaN( time ), 'DataPoint time is ' + time );
     assert && assert( time !== 0 || position.x === 0, 'Time is ' + time + 'but x is ' + position.x );
 
-    // @public - a datapoint may be part of multiple trajectories, so this is to keep track of how many are
+    // @public - a data point may be part of multiple trajectories, so this is to keep track of how many are
     // still using it, so we know when to dispose it.
     this.numberOfOtherTrajectoriesUsingSelf = 0;
 
