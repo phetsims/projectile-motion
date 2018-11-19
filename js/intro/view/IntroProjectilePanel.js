7afe6baa0d266e14046eeb90e94409aec93f7752
samreid <reids@colorado.edu>
2018-11-19 12:55:08 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/intro/view/IntroProjectilePanel.js
+++ js/intro/view/IntroProjectilePanel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2017, University of Colorado Boulder
+// Copyright 2016-2018, University of Colorado Boulder
 
 /**
  * Control panel that allows users to choose what kind of projectile to fire
