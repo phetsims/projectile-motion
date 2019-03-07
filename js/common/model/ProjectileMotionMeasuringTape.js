204d7dc070085c8e69ad0b5bf4e01e61d1998fed
samreid <reids@colorado.edu>
2019-03-07 16:05:48 -0700

    Use Vector2Property, see https://github.com/phetsims/dot/issues/88

--- js/common/model/ProjectileMotionMeasuringTape.js
+++ js/common/model/ProjectileMotionMeasuringTape.js
@@ -12,8 +12,8 @@ define( function( require ) {
   var BooleanProperty = require( 'AXON/BooleanProperty' );
   var inherit = require( 'PHET_CORE/inherit' );
   var projectileMotion = require( 'PROJECTILE_MOTION/projectileMotion' );
-  var Property = require( 'AXON/Property' );
   var Vector2 = require( 'DOT/Vector2' );
+  var Vector2Property = require( 'DOT/Vector2Property' );
 
   /**
    * @constructor
@@ -21,10 +21,10 @@ define( function( require ) {
   function ProjectileMotionMeasuringTape() {
 
     // @public - Base (start of tape from the container) position
-    this.basePositionProperty = new Property( new Vector2( 0, 0 ) );
+    this.basePositionProperty = new Vector2Property( new Vector2( 0, 0 ) );
 
     // @public - Tip (end of measuring tape) position
-    this.tipPositionProperty = new Property( new Vector2( 1, 0 ) );
+    this.tipPositionProperty = new Vector2Property( new Vector2( 1, 0 ) );
 
     // @public - Whether the measuring tape is out in the play area (false when in the toolbox)
     this.isActiveProperty = new BooleanProperty( false );
