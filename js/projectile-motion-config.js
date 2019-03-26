3916ffb7e29ecd73cdb1cb7d091104446ab38a23
samreid <reids@colorado.edu>
2019-03-26 16:35:24 -0600

    Add "twixt" to "phetLibs", see https://github.com/phetsims/phet-io/issues/1446

--- js/projectile-motion-config.js
+++ js/projectile-motion-config.js
@@ -40,7 +40,8 @@ require.config( {
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
     TAMBO: '../../tambo/js',
-    TANDEM: '../../tandem/js'
+    TANDEM: '../../tandem/js',
+    TWIXT: '../../twixt/js'
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
