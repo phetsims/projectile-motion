70b6bee1039b298a4d1fb127c8b103d956ceb611
phet-dev <phetadmin@gmail.com>
2020-01-03 03:24:51 -0700

    update copyright dates from daily grunt work

--- js/lab/view/KeypadLayer.js
+++ js/lab/view/KeypadLayer.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * KeypadLayer handles creation and management of a modal keypad.
