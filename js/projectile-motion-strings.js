ec941c825fb5a0a99480615a174c33a86f42e97c
samreid <reids@colorado.edu>
2020-02-27 12:07:59 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/projectile-motion-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import projectileMotion from './projectileMotion.js';
+
+const projectileMotionStrings = getStringModule( 'PROJECTILE_MOTION' );
+
+projectileMotion.register( 'projectileMotionStrings', projectileMotionStrings );
+
+export default projectileMotionStrings;
