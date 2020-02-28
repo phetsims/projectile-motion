f4f8013ff1dc9c20b3d3bf74a23fbff834c76038
phet-dev <phetadmin@gmail.com>
2020-02-28 03:44:58 -0700

    update copyright dates from daily grunt work

--- js/common/model/TrajectoryIO.js
+++ js/common/model/TrajectoryIO.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * IO type for Trajectory
--- js/vectors/view/VectorsIconNode.js
+++ js/vectors/view/VectorsIconNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * icon node for the 'Vectors' screen
