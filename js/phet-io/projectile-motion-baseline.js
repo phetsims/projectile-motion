77d71342b8cb37ea7df3cbe0017186b736bbb2e2
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:01 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/projectile-motion-baseline.js
+++ js/phet-io/projectile-motion-baseline.js
@@ -8636,7 +8636,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "projectileMotion.general.view.simNameProperty": {
+    "projectileMotion.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
