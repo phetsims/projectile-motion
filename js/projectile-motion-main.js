c93dbcdd4ab4aaea00b1ce0ee11ebde386da1cbc
zepumph <michael.kauzmann@colorado.edu>
2020-05-07 12:12:45 -0800

    Convert SimLauncher to es6 class, rename to simLauncher.js, https://github.com/phetsims/joist/issues/630

--- js/projectile-motion-main.js
+++ js/projectile-motion-main.js
@@ -7,7 +7,7 @@
  */
 
 import Sim from '../../joist/js/Sim.js';
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import DragScreen from './drag/DragScreen.js';
 import IntroScreen from './intro/IntroScreen.js';
@@ -27,7 +27,7 @@ const simOptions = {
   }
 };
 
-SimLauncher.launch( function() {
+simLauncher.launch( function() {
   const sim = new Sim( projectileMotionTitleString, [
     new IntroScreen( Tandem.ROOT.createTandem( 'introScreen' ) ),
     new VectorsScreen( Tandem.ROOT.createTandem( 'vectorsScreen' ) ),
