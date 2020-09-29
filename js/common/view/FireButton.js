da870e4ca8cd9ff2ed212c5150616edf579b5a3e
pixelzoom <cmalley@pixelzoom.com>
2020-09-29 17:35:51 -0600

    convert RectangularPushButton subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/FireButton.js
+++ js/common/view/FireButton.js
@@ -6,33 +6,32 @@
  * @author Andrea Lin (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
 import fireImage from '../../../mipmaps/fire_button_png.js';
 import projectileMotion from '../../projectileMotion.js';
 
-/**
- * @param {Object} [options]
- * @constructor
- */
-function FireButton( options ) {
-  options = merge( {
-    baseColor: 'rgb( 255, 0, 0 )', // red
-    iconWidth: 20 // width of icon, used for scaling, the aspect ratio will determine height
-  }, options );
+class FireButton extends RectangularPushButton {
 
-  assert && assert( !options.content, 'this type sets its own content' );
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
 
-  // fire button icon
-  options.content = new Image( fireImage );
-  options.content.scale( options.iconWidth / options.content.width );
+    options = merge( {
+      baseColor: 'rgb( 255, 0, 0 )', // red
+      iconWidth: 20 // width of icon, used for scaling, the aspect ratio will determine height
+    }, options );
 
-  RectangularPushButton.call( this, options );
+    // fire button icon
+    assert && assert( !options.content, 'this type sets its own content' );
+    options.content = new Image( fireImage );
+    options.content.scale( options.iconWidth / options.content.width );
+
+    super( options );
+  }
 }
 
 projectileMotion.register( 'FireButton', FireButton );
-
-inherit( RectangularPushButton, FireButton );
 export default FireButton;
