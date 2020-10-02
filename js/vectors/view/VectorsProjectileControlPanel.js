507b607a5e4e49ed54d653a4154c7af1fd51ba78
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 21:18:31 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/vectors/view/VectorsProjectileControlPanel.js
+++ js/vectors/view/VectorsProjectileControlPanel.js
@@ -7,7 +7,6 @@
  * @author Andrea Lin (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
 import Line from '../../../../scenery/js/nodes/Line.js';
@@ -19,8 +18,8 @@ import ProjectileMotionConstants from '../../common/ProjectileMotionConstants.js
 import AirResistanceControl from '../../common/view/AirResistanceControl.js';
 import ArrowlessNumberControl from '../../common/view/ArrowlessNumberControl.js';
 import ProjectileObjectViewFactory from '../../common/view/ProjectileObjectViewFactory.js';
-import projectileMotionStrings from '../../projectileMotionStrings.js';
 import projectileMotion from '../../projectileMotion.js';
+import projectileMotionStrings from '../../projectileMotionStrings.js';
 
 const cannonballString = projectileMotionStrings.cannonball;
 const diameterString = projectileMotionStrings.diameter;
@@ -33,96 +32,96 @@ const LABEL_OPTIONS = ProjectileMotionConstants.PANEL_LABEL_OPTIONS;
 
 const DRAG_OBJECT_DISPLAY_DIAMETER = 24;
 
-/**
- * @param {Property.<ProjectiLeObjectType} selectedObjectTypeProperty
- * @param {Property.<number>} projectileDiameterProperty
- * @param {Property.<number>} projectileMassProperty
- * @param {Property.<boolean>} airResistanceOnProperty - whether air resistance is on
- * @param {Property.<number>} projectileDragCoefficientProperty
- * @param {Object} [options]
- * @constructor
- */
-function VectorsProjectileControlPanel( selectedObjectTypeProperty,
-                                        projectileDiameterProperty,
-                                        projectileMassProperty,
-                                        airResistanceOnProperty,
-                                        projectileDragCoefficientProperty,
-                                        options ) {
+class VectorsProjectileControlPanel extends Panel {
 
-  // The first object is a placeholder so none of the others get mutated
-  // The second object is the default, in the constants files
-  // The third object is options specific to this panel, which overrides the defaults
-  // The fourth object is options given at time of construction, which overrides all the others
-  options = merge( {}, ProjectileMotionConstants.RIGHTSIDE_PANEL_OPTIONS, {
-    textDisplayWidth: 45,
-    tandem: Tandem.REQUIRED
-  }, options );
+  /**
+   * @param {Property.<ProjectiLeObjectType} selectedObjectTypeProperty
+   * @param {Property.<number>} projectileDiameterProperty
+   * @param {Property.<number>} projectileMassProperty
+   * @param {Property.<boolean>} airResistanceOnProperty - whether air resistance is on
+   * @param {Property.<number>} projectileDragCoefficientProperty
+   * @param {Object} [options]
+   */
+  constructor( selectedObjectTypeProperty,
+               projectileDiameterProperty,
+               projectileMassProperty,
+               airResistanceOnProperty,
+               projectileDragCoefficientProperty,
+               options ) {
 
-  // drag coefficient object shape view
-  const objectView = ProjectileObjectViewFactory.createCustom( DRAG_OBJECT_DISPLAY_DIAMETER, ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT );
-  const objectDisplay = new HBox( {
-    spacing: options.xMargin,
-    children: [
-      new Text( cannonballString, merge( {}, LABEL_OPTIONS, {
-        maxWidth: options.minWidth - 3 * options.xMargin - objectView.width
-      } ) ),
-      objectView
-    ]
-  } );
+    // The first object is a placeholder so none of the others get mutated
+    // The second object is the default, in the constants files
+    // The third object is options specific to this panel, which overrides the defaults
+    // The fourth object is options given at time of construction, which overrides all the others
+    options = merge( {}, ProjectileMotionConstants.RIGHTSIDE_PANEL_OPTIONS, {
+      textDisplayWidth: 45,
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const selectedObjectType = selectedObjectTypeProperty.get();
+    // drag coefficient object shape view
+    const objectView = ProjectileObjectViewFactory.createCustom( DRAG_OBJECT_DISPLAY_DIAMETER, ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT );
+    const objectDisplay = new HBox( {
+      spacing: options.xMargin,
+      children: [
+        new Text( cannonballString, merge( {}, LABEL_OPTIONS, {
+          maxWidth: options.minWidth - 3 * options.xMargin - objectView.width
+        } ) ),
+        objectView
+      ]
+    } );
 
-  const diameterNumberControl = new ArrowlessNumberControl(
-    diameterString,
-    mString,
-    projectileDiameterProperty,
-    selectedObjectType.diameterRange,
-    selectedObjectType.diameterRound, {
-      containerWidth: options.minWidth,
-      xMargin: options.xMargin,
-      numberDisplayMaxWidth: options.numberDisplayMaxWidth,
-      tandem: options.tandem.createTandem( 'diameterNumberControl' ),
-      phetioDocumentation: 'UI control to adjust the diameter of the projectile'
-    }
-  );
+    const selectedObjectType = selectedObjectTypeProperty.get();
 
-  const massNumberControl = new ArrowlessNumberControl(
-    massString,
-    kgString,
-    projectileMassProperty,
-    selectedObjectType.massRange,
-    selectedObjectType.massRound, {
-      containerWidth: options.minWidth,
-      xMargin: options.xMargin,
-      numberDisplayMaxWidth: options.numberDisplayMaxWidth,
-      tandem: options.tandem.createTandem( 'massNumberControl' ),
-      phetioDocumentation: 'UI control to adjust the mass of the projectile'
-    }
-  );
-
-  // The contents of the control panel
-  const content = new VBox( {
-    align: 'left',
-    spacing: options.controlsVerticalSpace,
-    children: [
-      objectDisplay,
-      diameterNumberControl,
-      massNumberControl,
-      new Line( 0, 0, options.minWidth - 2 * options.xMargin, 0, { stroke: 'gray' } ),
-      new AirResistanceControl( airResistanceOnProperty, projectileDragCoefficientProperty, {
-        labelOptions: LABEL_OPTIONS,
-        minWidth: options.minWidth,
+    const diameterNumberControl = new ArrowlessNumberControl(
+      diameterString,
+      mString,
+      projectileDiameterProperty,
+      selectedObjectType.diameterRange,
+      selectedObjectType.diameterRound, {
+        containerWidth: options.minWidth,
         xMargin: options.xMargin,
-        spacing: options.controlsVerticalSpace,
-        tandem: options.tandem.createTandem( 'airResistanceControl' )
-      } )
-    ]
-  } );
+        numberDisplayMaxWidth: options.numberDisplayMaxWidth,
+        tandem: options.tandem.createTandem( 'diameterNumberControl' ),
+        phetioDocumentation: 'UI control to adjust the diameter of the projectile'
+      }
+    );
 
-  Panel.call( this, content, options );
+    const massNumberControl = new ArrowlessNumberControl(
+      massString,
+      kgString,
+      projectileMassProperty,
+      selectedObjectType.massRange,
+      selectedObjectType.massRound, {
+        containerWidth: options.minWidth,
+        xMargin: options.xMargin,
+        numberDisplayMaxWidth: options.numberDisplayMaxWidth,
+        tandem: options.tandem.createTandem( 'massNumberControl' ),
+        phetioDocumentation: 'UI control to adjust the mass of the projectile'
+      }
+    );
+
+    // The contents of the control panel
+    const content = new VBox( {
+      align: 'left',
+      spacing: options.controlsVerticalSpace,
+      children: [
+        objectDisplay,
+        diameterNumberControl,
+        massNumberControl,
+        new Line( 0, 0, options.minWidth - 2 * options.xMargin, 0, { stroke: 'gray' } ),
+        new AirResistanceControl( airResistanceOnProperty, projectileDragCoefficientProperty, {
+          labelOptions: LABEL_OPTIONS,
+          minWidth: options.minWidth,
+          xMargin: options.xMargin,
+          spacing: options.controlsVerticalSpace,
+          tandem: options.tandem.createTandem( 'airResistanceControl' )
+        } )
+      ]
+    } );
+
+    super( content, options );
+  }
 }
 
 projectileMotion.register( 'VectorsProjectileControlPanel', VectorsProjectileControlPanel );
-
-inherit( Panel, VectorsProjectileControlPanel );
 export default VectorsProjectileControlPanel;
--- js/vectors/view/VectorsVectorsControlPanel.js
+++ js/vectors/view/VectorsVectorsControlPanel.js
@@ -6,7 +6,6 @@
  * @author Andrea Lin (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
@@ -18,8 +17,8 @@ import VerticalAquaRadioButtonGroup from '../../../../sun/js/VerticalAquaRadioBu
 import Tandem from '../../../../tandem/js/Tandem.js';
 import ProjectileMotionConstants from '../../common/ProjectileMotionConstants.js';
 import VectorsDisplayEnumeration from '../../common/view/VectorsDisplayEnumeration.js';
-import projectileMotionStrings from '../../projectileMotionStrings.js';
 import projectileMotion from '../../projectileMotion.js';
+import projectileMotionStrings from '../../projectileMotionStrings.js';
 
 const accelerationVectorsString = projectileMotionStrings.accelerationVectors;
 const componentsString = projectileMotionStrings.components;
@@ -33,100 +32,99 @@ const VELOCITY_VECTOR_ICON = ProjectileMotionConstants.VELOCITY_VECTOR_ICON;
 const ACCELERATION_VECTOR_ICON = ProjectileMotionConstants.ACCELERATION_VECTOR_ICON;
 const FORCE_VECTOR_ICON = ProjectileMotionConstants.FORCE_VECTOR_ICON;
 
-/**
- * @param {VectorVisibilityProperties} vectorVisibilityProperties - Properties that determine which vectors are shown
- * @param {Object} [options]
- * @constructor
- */
-function VectorsVectorsControlPanel( vectorVisibilityProperties, options ) {
+class VectorsVectorsControlPanel extends Panel {
+  /**
+   * @param {VectorVisibilityProperties} vectorVisibilityProperties - Properties that determine which vectors are shown
+   * @param {Object} [options]
+   */
+  constructor( vectorVisibilityProperties, options ) {
 
-  // The first object is a placeholder so none of the others get mutated
-  // The second object is the default, in the constants files
-  // The third object is options specific to this panel, which overrides the defaults
-  // The fourth object is options given at time of construction, which overrides all the others
-  options = merge( {}, ProjectileMotionConstants.RIGHTSIDE_PANEL_OPTIONS, {
-    align: 'left',
-    tandem: Tandem.REQUIRED
-  }, options );
+    // The first object is a placeholder so none of the others get mutated
+    // The second object is the default, in the constants files
+    // The third object is options specific to this panel, which overrides the defaults
+    // The fourth object is options given at time of construction, which overrides all the others
+    options = merge( {}, ProjectileMotionConstants.RIGHTSIDE_PANEL_OPTIONS, {
+      align: 'left',
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const checkboxOptions = {
-    maxWidth: options.minWidth - 3 * options.xMargin - VELOCITY_VECTOR_ICON.width,
-    boxWidth: 18
-  };
+    const checkboxOptions = {
+      maxWidth: options.minWidth - 3 * options.xMargin - VELOCITY_VECTOR_ICON.width,
+      boxWidth: 18
+    };
 
-  const totalLabel = new Text( totalString, LABEL_OPTIONS );
-  const componentsLabel = new Text( componentsString, LABEL_OPTIONS );
+    const totalLabel = new Text( totalString, LABEL_OPTIONS );
+    const componentsLabel = new Text( componentsString, LABEL_OPTIONS );
 
-  const totalOrComponentsRadioButtonGroup = new VerticalAquaRadioButtonGroup( vectorVisibilityProperties.vectorsDisplayProperty, [ {
-    node: totalLabel,
-    tandemName: 'totalRadioButton',
-    value: VectorsDisplayEnumeration.TOTAL
-  }, {
-    node: componentsLabel,
-    tandemName: 'componentsRadioButton',
-    value: VectorsDisplayEnumeration.COMPONENTS
-  } ], {
-    radioButtonOptions: { radius: 8 },
-    spacing: 10,     // vertical spacing between each radio button
-    touchAreaXDilation: 5,
-    maxWidth: checkboxOptions.maxWidth,
-    tandem: options.tandem.createTandem( 'vectorsDisplayRadioButtonGroup' ),
-    phetioDocumentation: 'Radio button group to select what type of vectors are displayed with a flying projectile'
-  } );
+    const totalOrComponentsRadioButtonGroup = new VerticalAquaRadioButtonGroup( vectorVisibilityProperties.vectorsDisplayProperty, [ {
+      node: totalLabel,
+      tandemName: 'totalRadioButton',
+      value: VectorsDisplayEnumeration.TOTAL
+    }, {
+      node: componentsLabel,
+      tandemName: 'componentsRadioButton',
+      value: VectorsDisplayEnumeration.COMPONENTS
+    } ], {
+      radioButtonOptions: { radius: 8 },
+      spacing: 10,     // vertical spacing between each radio button
+      touchAreaXDilation: 5,
+      maxWidth: checkboxOptions.maxWidth,
+      tandem: options.tandem.createTandem( 'vectorsDisplayRadioButtonGroup' ),
+      phetioDocumentation: 'Radio button group to select what type of vectors are displayed with a flying projectile'
+    } );
 
-  const velocityLabel = new Text( velocityVectorsString, LABEL_OPTIONS );
-  const velocityCheckboxContent = new HBox( {
-    spacing: options.xMargin,
-    children: [
-      velocityLabel,
-      new Node( { children: [ VELOCITY_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
-    ]
-  } );
-  const velocityCheckbox = new Checkbox( velocityCheckboxContent, vectorVisibilityProperties.velocityVectorsOnProperty,
-    merge( { tandem: options.tandem.createTandem( 'velocityCheckbox' ) }, checkboxOptions )
-  );
+    const velocityLabel = new Text( velocityVectorsString, LABEL_OPTIONS );
+    const velocityCheckboxContent = new HBox( {
+      spacing: options.xMargin,
+      children: [
+        velocityLabel,
+        new Node( { children: [ VELOCITY_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
+      ]
+    } );
+    const velocityCheckbox = new Checkbox( velocityCheckboxContent, vectorVisibilityProperties.velocityVectorsOnProperty,
+      merge( { tandem: options.tandem.createTandem( 'velocityCheckbox' ) }, checkboxOptions )
+    );
 
-  const accelerationLabel = new Text( accelerationVectorsString, LABEL_OPTIONS );
-  const accelerationCheckboxContent = new HBox( {
-    spacing: options.xMargin,
-    children: [
-      accelerationLabel,
-      new Node( { children: [ ACCELERATION_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
-    ]
-  } );
-  const accelerationCheckbox = new Checkbox( accelerationCheckboxContent,
-    vectorVisibilityProperties.accelerationVectorsOnProperty,
-    merge( { tandem: options.tandem.createTandem( 'accelerationCheckbox' ) }, checkboxOptions )
-  );
+    const accelerationLabel = new Text( accelerationVectorsString, LABEL_OPTIONS );
+    const accelerationCheckboxContent = new HBox( {
+      spacing: options.xMargin,
+      children: [
+        accelerationLabel,
+        new Node( { children: [ ACCELERATION_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
+      ]
+    } );
+    const accelerationCheckbox = new Checkbox( accelerationCheckboxContent,
+      vectorVisibilityProperties.accelerationVectorsOnProperty,
+      merge( { tandem: options.tandem.createTandem( 'accelerationCheckbox' ) }, checkboxOptions )
+    );
 
-  const forceLabel = new Text( forceVectorsString, LABEL_OPTIONS );
-  const forceCheckboxContent = new HBox( {
-    spacing: options.xMargin,
-    children: [
-      forceLabel,
-      new Node( { children: [ FORCE_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
-    ]
-  } );
-  const forceCheckbox = new Checkbox( forceCheckboxContent, vectorVisibilityProperties.forceVectorsOnProperty,
-    merge( { tandem: options.tandem.createTandem( 'forceCheckbox' ) }, checkboxOptions )
-  );
+    const forceLabel = new Text( forceVectorsString, LABEL_OPTIONS );
+    const forceCheckboxContent = new HBox( {
+      spacing: options.xMargin,
+      children: [
+        forceLabel,
+        new Node( { children: [ FORCE_VECTOR_ICON ] } ) // so that HBox transforms the intermediary Node
+      ]
+    } );
+    const forceCheckbox = new Checkbox( forceCheckboxContent, vectorVisibilityProperties.forceVectorsOnProperty,
+      merge( { tandem: options.tandem.createTandem( 'forceCheckbox' ) }, checkboxOptions )
+    );
 
-  // The contents of the control panel
-  const content = new VBox( {
-    align: 'left',
-    spacing: options.controlsVerticalSpace,
-    children: [
-      totalOrComponentsRadioButtonGroup,
-      velocityCheckbox,
-      accelerationCheckbox,
-      forceCheckbox
-    ]
-  } );
+    // The contents of the control panel
+    const content = new VBox( {
+      align: 'left',
+      spacing: options.controlsVerticalSpace,
+      children: [
+        totalOrComponentsRadioButtonGroup,
+        velocityCheckbox,
+        accelerationCheckbox,
+        forceCheckbox
+      ]
+    } );
 
-  Panel.call( this, content, options );
+    super( content, options );
+  }
 }
 
 projectileMotion.register( 'VectorsVectorsControlPanel', VectorsVectorsControlPanel );
-
-inherit( Panel, VectorsVectorsControlPanel );
 export default VectorsVectorsControlPanel;
