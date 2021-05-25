97a9734529c5ccb513cec0d7b203e521d0c47a51
phet-dev <phetadmin@gmail.com>
2021-05-25 16:02:50 -0600

    update copyright dates from daily grunt work

--- js/common/view/AirResistanceControl.js
+++ js/common/view/AirResistanceControl.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2021, University of Colorado Boulder
 
 /**
  * Scenery node that shows the background, including the sky, grass, and road.
--- js/lab/view/KeypadLayer.js
+++ js/lab/view/KeypadLayer.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020, University of Colorado Boulder
+// Copyright 2017-2021, University of Colorado Boulder
 
 /**
  * KeypadLayer handles creation and management of a modal keypad.
