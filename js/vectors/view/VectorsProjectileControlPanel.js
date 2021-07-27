f8187c5366cd587179b3851432b7e392d6b4cd51
phet-dev <phetadmin@gmail.com>
2021-07-27 03:37:58 -0600

    update copyright dates from daily grunt work

--- js/vectors/view/VectorsProjectileControlPanel.js
+++ js/vectors/view/VectorsProjectileControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * Control panel that allows users to alter Properties of the projectile fired.
