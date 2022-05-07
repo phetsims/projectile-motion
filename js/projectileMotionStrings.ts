f88e134c8ea91b5384001aafe21a4502f37674b4
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:55 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/projectileMotionStrings.ts
+++ js/projectileMotionStrings.ts
@@ -9,52 +9,52 @@ import projectileMotion from './projectileMotion.js';
 
 type StringsType = {
   'projectile-motion': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'lab': string,
-    'intro': string,
-    'drag': string,
-    'vectors': string
-  },
-  'pattern0Value1Units': string,
-  'pattern0Value1UnitsWithSpace': string,
-  'custom': string,
-  'enter': string,
-  'angle': string,
-  'height': string,
-  'gravity': string,
-  'm': string,
-  'meters': string,
-  'mass': string,
-  'kg': string,
-  'diameter': string,
-  'initialSpeed': string,
-  'altitude': string,
-  'speed': string,
-  'metersPerSecond': string,
-  'metersPerSecondSquared': string,
-  'dragCoefficient': string,
-  'airResistance': string,
-  'time': string,
-  's': string,
-  'range': string,
-  'cannonball': string,
-  'pumpkin': string,
-  'baseball': string,
-  'car': string,
-  'football': string,
-  'human': string,
-  'piano': string,
-  'golfBall': string,
-  'tankShell': string,
-  'initialValues': string,
-  'velocityVectors': string,
-  'accelerationVectors': string,
-  'forceVectors': string,
-  'total': string,
-  'components': string,
-  'rangeMessage': string
+    'lab': string;
+    'intro': string;
+    'drag': string;
+    'vectors': string;
+  };
+  'pattern0Value1Units': string;
+  'pattern0Value1UnitsWithSpace': string;
+  'custom': string;
+  'enter': string;
+  'angle': string;
+  'height': string;
+  'gravity': string;
+  'm': string;
+  'meters': string;
+  'mass': string;
+  'kg': string;
+  'diameter': string;
+  'initialSpeed': string;
+  'altitude': string;
+  'speed': string;
+  'metersPerSecond': string;
+  'metersPerSecondSquared': string;
+  'dragCoefficient': string;
+  'airResistance': string;
+  'time': string;
+  's': string;
+  'range': string;
+  'cannonball': string;
+  'pumpkin': string;
+  'baseball': string;
+  'car': string;
+  'football': string;
+  'human': string;
+  'piano': string;
+  'golfBall': string;
+  'tankShell': string;
+  'initialValues': string;
+  'velocityVectors': string;
+  'accelerationVectors': string;
+  'forceVectors': string;
+  'total': string;
+  'components': string;
+  'rangeMessage': string;
 };
 
 const projectileMotionStrings = getStringModule( 'PROJECTILE_MOTION' ) as StringsType;
