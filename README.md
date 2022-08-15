8c71179cb5c364baa0ebe078a4ef35ad5cb94c23
Sam Reid <reids@colorado.edu>
2022-08-15 09:29:45 -0600

    Run grunt update, see https://github.com/phetsims/chipper/issues/1157

--- README.md
+++ README.md
@@ -66,7 +66,8 @@ that will automatically transpile new or changed files.
 
 (1) Change directory to the simulation directory: `cd ../projectile-motion`
 
-(2) Build the sim: `grunt --brands=adapted-from-phet`
+(2) Build the sim: `grunt --brands=adapted-from-phet`. It is safe to ignore warnings like `>> WARNING404: Skipping potentially non-public dependency`,
+which indicate that non-public PhET-iO code is not being included in the build.
 
 (3) Open in the browser: `http://localhost/projectile-motion/build/adapted-from-phet/projectile-motion_en_adapted-from-phet.html` (You will probably need to modify this URL based on your HTTP port and relative path.)
 
--- js/projectileMotionStrings.ts
+++ js/projectileMotionStrings.ts
@@ -4,64 +4,62 @@
  * Auto-generated from modulify, DO NOT manually modify.
  */
 /* eslint-disable */
-import getStringModule from "../../chipper/js/getStringModule.js";
-import projectileMotion from "./projectileMotion.js";
+import getStringModule from '../../chipper/js/getStringModule.js';
+import projectileMotion from './projectileMotion.js';
 
 type StringsType = {
-  "projectile-motion": {
-    title: string;
+  'projectile-motion': {
+    'title': string;
   };
-  screen: {
-    lab: string;
-    intro: string;
-    drag: string;
-    vectors: string;
-    stats: string;
+  'screen': {
+    'lab': string;
+    'intro': string;
+    'drag': string;
+    'vectors': string;
+    'stats': string;
   };
-  pattern0Value1Units: string;
-  pattern0Value1UnitsWithSpace: string;
-  custom: string;
-  enter: string;
-  angle: string;
-  height: string;
-  gravity: string;
-  m: string;
-  meters: string;
-  mass: string;
-  kg: string;
-  diameter: string;
-  initialSpeed: string;
-  altitude: string;
-  speed: string;
-  metersPerSecond: string;
-  metersPerSecondSquared: string;
-  dragCoefficient: string;
-  airResistance: string;
-  time: string;
-  s: string;
-  range: string;
-  cannonball: string;
-  pumpkin: string;
-  baseball: string;
-  car: string;
-  football: string;
-  human: string;
-  piano: string;
-  golfBall: string;
-  tankShell: string;
-  initialValues: string;
-  velocityVectors: string;
-  accelerationVectors: string;
-  forceVectors: string;
-  total: string;
-  components: string;
-  rangeMessage: string;
+  'pattern0Value1Units': string;
+  'pattern0Value1UnitsWithSpace': string;
+  'custom': string;
+  'enter': string;
+  'angle': string;
+  'height': string;
+  'gravity': string;
+  'm': string;
+  'meters': string;
+  'mass': string;
+  'kg': string;
+  'diameter': string;
+  'initialSpeed': string;
+  'altitude': string;
+  'speed': string;
+  'metersPerSecond': string;
+  'metersPerSecondSquared': string;
+  'dragCoefficient': string;
+  'airResistance': string;
+  'time': string;
+  's': string;
+  'range': string;
+  'cannonball': string;
+  'pumpkin': string;
+  'baseball': string;
+  'car': string;
+  'football': string;
+  'human': string;
+  'piano': string;
+  'golfBall': string;
+  'tankShell': string;
+  'initialValues': string;
+  'velocityVectors': string;
+  'accelerationVectors': string;
+  'forceVectors': string;
+  'total': string;
+  'components': string;
+  'rangeMessage': string;
 };
 
-const projectileMotionStrings = getStringModule(
-  "PROJECTILE_MOTION"
-) as StringsType;
+const projectileMotionStrings = getStringModule( 'PROJECTILE_MOTION' ) as StringsType;
 
-projectileMotion.register("projectileMotionStrings", projectileMotionStrings);
+projectileMotion.register( 'projectileMotionStrings', projectileMotionStrings );
 
 export default projectileMotionStrings;
