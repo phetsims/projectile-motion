a0beab7657892cda6fe13af05b2c1b7cbb3b54b3
Matthew Blackman <matthewblackman1@gmail.com>
2022-10-26 16:45:07 -0400

    Convert to Typescript - https://github.com/phetsims/projectile-motion/issues/306

--- js/common/model/ProjectileMotionMeasuringTape.ts
+++ js/common/model/ProjectileMotionMeasuringTape.ts
@@ -1,37 +1,39 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Model for the measuring tape in projectile motion.
  *
  * @author Andrea Lin (PhET Interactive Simulations)
+ * @author Matthew Blackman (PhET Interactive Simulations)
  */
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import Property from '../../../../axon/js/Property.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 import projectileMotion from '../../projectileMotion.js';
 
 class ProjectileMotionMeasuringTape {
-  /**
-   * @param {Tandem} tandem
-   */
-  constructor( tandem ) {
 
-    // @public
+  public readonly basePositionProperty: Property<Vector2>;
+  public readonly tipPositionProperty: Property<Vector2>;
+  public readonly isActiveProperty: Property<boolean>;
+
+  public constructor( tandem: Tandem ) {
+
     this.basePositionProperty = new Vector2Property( new Vector2( 0, 0 ), {
       tandem: tandem.createTandem( 'basePositionProperty' ),
       units: 'm',
       phetioDocumentation: 'Base (start of tape from the container) position'
     } );
 
-    // @public -
     this.tipPositionProperty = new Vector2Property( new Vector2( 1, 0 ), {
       tandem: tandem.createTandem( 'tipPositionProperty' ),
       units: 'm',
       phetioDocumentation: 'Tip (end of measuring tape) position'
     } );
 
-    // @public
     this.isActiveProperty = new BooleanProperty( false, {
       tandem: tandem.createTandem( 'isActiveProperty' ),
       phetioDocumentation: 'Whether the measuring tape is out in the play area (false when in the toolbox)'
@@ -40,10 +42,8 @@ class ProjectileMotionMeasuringTape {
 
   /**
    * Reset these Properties
-   * @public
-   * @override
    */
-  reset() {
+  public reset(): void {
     this.basePositionProperty.reset();
     this.tipPositionProperty.reset();
     this.isActiveProperty.reset();
