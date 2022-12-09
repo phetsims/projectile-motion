0f82168d5befc43b9dc986d40fff20563ab26012
Matthew Blackman <matthewblackman1@gmail.com>
2022-12-09 11:26:35 -0500

    Use PATH_MAX_OPACITY as trajectory default opacity - see https://github.com/phetsims/projectile-motion/issues/314

--- js/common/view/TrajectoryNode.js
+++ js/common/view/TrajectoryNode.js
@@ -185,7 +185,7 @@ class TrajectoryNode extends Node {
     function updateOpacity( rank ) {
       //if projectile opacity is set as constant (as in 'stats' screen)
       if ( constantTrajectoryOpacity ) {
-        pathsLayer.opacity = 0.1; // TODO: MOVE TO CONSTANTS, https://github.com/phetsims/projectile-motion/issues/314
+        pathsLayer.opacity = PATH_MAX_OPACITY;
       }
       else {
         const strength = ( maxNumberOfTrajectories - rank ) / maxNumberOfTrajectories;
