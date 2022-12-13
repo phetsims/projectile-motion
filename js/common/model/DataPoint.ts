504d95957f7893f967764bc3ba9c64388e551360
Matthew Blackman <matthewblackman1@gmail.com>
2022-12-13 11:23:50 -0500

    Convert DataPoint.ts - see https://github.com/phetsims/projectile-motion/issues/306

--- js/common/model/DataPoint.ts
+++ js/common/model/DataPoint.ts
@@ -7,35 +7,61 @@
  */
 
 import Vector2 from '../../../../dot/js/Vector2.js';
-import merge from '../../../../phet-core/js/merge.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import BooleanIO from '../../../../tandem/js/types/BooleanIO.js';
 import IOType from '../../../../tandem/js/types/IOType.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
+import { CompositeSchema } from '../../../../tandem/js/types/StateSchema.js';
 import projectileMotion from '../../projectileMotion.js';
 
+type DataPointOptions = {
+  apex?: boolean;
+  reachedGround?: boolean;
+};
+
+type StateObject = {
+  time?: number;
+  position?: Vector2;
+  airDensity?: number;
+  velocity?: Vector2;
+  acceleration?: Vector2;
+  dragForce?: Vector2;
+  forceGravity?: number;
+  apex?: boolean;
+  reachedGround?: boolean;
+};
+
 class DataPoint {
+  public readonly time: number;
+  public readonly position: Vector2;
+  public readonly airDensity: number;
+  public readonly velocity: Vector2;
+  public readonly acceleration: Vector2;
+  public readonly dragForce: Vector2;
+  public readonly forceGravity: number;
+  public readonly apex: boolean;
+  public readonly reachedGround: boolean;
 
   /**
-   * @param {number} time - total time since fire at this point on the trajectory, in s
-   * @param {Vector2} position - position of the data point, with x and y, also called range and height, in m
-   * @param {number} airDensity - air density of the atmosphere at this point, in kg/cu m
-   * @param {Vector2} velocity - velocity at this point, magnitude in m/s
-   * @param {Vector2} acceleration - acceleration at this point, magnitude in m/s^2
-   * @param {Vector2} dragForce - drag force at this point, magnitude in N
-   * @param {number} forceGravity - force of gravity, in N
-   * @param {Object} [options]
+   * @param time - total time since fire at this point on the trajectory, in s
+   * @param position - position of the data point, with x and y, also called range and height, in m
+   * @param airDensity - air density of the atmosphere at this point, in kg/cu m
+   * @param velocity - velocity at this point, magnitude in m/s
+   * @param acceleration - acceleration at this point, magnitude in m/s^2
+   * @param dragForce - drag force at this point, magnitude in N
+   * @param forceGravity - force of gravity, in N
+   * @param [providedOptions]
    */
-  constructor( time, position, airDensity, velocity, acceleration, dragForce, forceGravity, options ) {
+  public constructor( time: number, position: Vector2, airDensity: number, velocity: Vector2, acceleration: Vector2, dragForce: Vector2, forceGravity: number, providedOptions?: DataPointOptions ) {
 
     assert && assert( !isNaN( time ), `DataPoint time is ${time}` );
     assert && assert( time !== 0 || position.x === 0, `Time is ${time}, but x is ${position.x}` );
 
-    options = merge( {
+    const options = optionize<DataPointOptions>()( {
       apex: false,
       reachedGround: false
-    }, options );
+    }, providedOptions );
 
-    // @public (read-only)
     this.time = time;
     this.position = position;
     this.airDensity = airDensity;
@@ -49,11 +75,8 @@ class DataPoint {
 
   /**
    * Whether this dataPoint is equal to given dataPoint
-   * @public
-   *
-   * @returns {boolean}
    */
-  equals( dataPoint ) {
+  public equals( dataPoint: DataPoint ) : boolean {
     return this.position.equals( dataPoint.position )
            && this.time === dataPoint.time
            && this.airDensity === dataPoint.airDensity
@@ -63,10 +86,7 @@ class DataPoint {
            && this.forceGravity === dataPoint.forceGravity;
   }
 
-  /**
-   * @public
-   */
-  dispose() {
+  public dispose() : void {
     this.position.freeToPool();
     this.velocity.freeToPool();
     this.acceleration.freeToPool();
@@ -75,10 +95,8 @@ class DataPoint {
 
   /**
    * Returns a map of state keys and their associated IOTypes, see IOType for details.
-   * @returns {Object.<string,IOType>}
-   * @public
    */
-  static get STATE_SCHEMA() {
+  public static get STATE_SCHEMA() : CompositeSchema {
     return {
       time: NumberIO,
       position: Vector2.Vector2IO,
@@ -94,40 +112,37 @@ class DataPoint {
 
   /**
    * Deserializes a DataPoint instance.
-   * @param {Object} stateObject - return value from toStateObject
-   * @returns {DataPoint}
-   * @public
    */
-  static fromStateObject( stateObject ) {
+  public static fromStateObject( stateObject : StateObject ) : DataPoint {
     return new DataPoint(
-      stateObject.time,
+      stateObject.time ? stateObject.time : 0,
       Vector2.Vector2IO.fromStateObject( stateObject.position ),
-      stateObject.airDensity,
+      stateObject.airDensity!,
       Vector2.Vector2IO.fromStateObject( stateObject.velocity ),
       Vector2.Vector2IO.fromStateObject( stateObject.acceleration ),
       Vector2.Vector2IO.fromStateObject( stateObject.dragForce ),
-      stateObject.forceGravity, {
+      stateObject.forceGravity ? stateObject.forceGravity : 0, {
         apex: stateObject.apex,
         reachedGround: stateObject.reachedGround
       } );
   }
-}
 
-DataPoint.DataPointIO = new IOType( 'DataPointIO', {
-  valueType: DataPoint,
-  stateSchema: DataPoint.STATE_SCHEMA,
-  fromStateObject: stateObject => DataPoint.fromStateObject( stateObject ),
-  documentation: 'A single data point on a projectile\'s trajectory, with the following data:<br><ul>' +
-                 '<li>time (seconds): The time of the data point</li>' +
-                 '<li>position (meters): the position of the point in model coordinates</li>' +
-                 '<li>airDensity (kg/m^3): the air density when the point was collected</li>' +
-                 '<li>velocity (m/s): the velocity of the projectile at the time when the point was collected</li>' +
-                 '<li>acceleration (m/s^2): the acceleration of the projectile at the time when the point was collected</li>' +
-                 '<li>dragForce (newtons): the acceleration of the projectile at the time when the point was collected</li>' +
-                 '<li>forceGravity (newtons): the acceleration of the projectile at the time when the point was collected</li>' +
-                 '<li>apex (boolean): if this data point was at the apex of a trajectory</li>' +
-                 '<li>reachedGround (boolean): if this data point was collected when the projectile was on the ground</li></ul>'
-} );
+  public static readonly DataPointIO = new IOType( 'DataPointIO', {
+    valueType: DataPoint,
+    stateSchema: DataPoint.STATE_SCHEMA,
+    fromStateObject: stateObject => DataPoint.fromStateObject( stateObject ),
+    documentation: 'A single data point on a projectile\'s trajectory, with the following data:<br><ul>' +
+                   '<li>time (seconds): The time of the data point</li>' +
+                   '<li>position (meters): the position of the point in model coordinates</li>' +
+                   '<li>airDensity (kg/m^3): the air density when the point was collected</li>' +
+                   '<li>velocity (m/s): the velocity of the projectile at the time when the point was collected</li>' +
+                   '<li>acceleration (m/s^2): the acceleration of the projectile at the time when the point was collected</li>' +
+                   '<li>dragForce (newtons): the acceleration of the projectile at the time when the point was collected</li>' +
+                   '<li>forceGravity (newtons): the acceleration of the projectile at the time when the point was collected</li>' +
+                   '<li>apex (boolean): if this data point was at the apex of a trajectory</li>' +
+                   '<li>reachedGround (boolean): if this data point was collected when the projectile was on the ground</li></ul>'
+  } );
+}
 
 projectileMotion.register( 'DataPoint', DataPoint );
 export default DataPoint;
