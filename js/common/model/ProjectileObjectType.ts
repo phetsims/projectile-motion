5894392bbefc931bd8b828bc1011c511953e0cc8
Matthew Blackman <matthewblackman1@gmail.com>
2022-12-16 14:27:25 -0500

    Convert ProjectileObjectType.ts - see https://github.com/phetsims/projectile-motion/issues/306

--- js/common/model/ProjectileObjectType.ts
+++ js/common/model/ProjectileObjectType.ts
@@ -7,14 +7,17 @@
  * @author Andrea Lin (PhET Interactive Simulations)
  */
 
-import Range from '../../../../dot/js/Range.js';
-import merge from '../../../../phet-core/js/merge.js';
-import PhetioObject from '../../../../tandem/js/PhetioObject.js';
+import Range, { RangeStateObject } from '../../../../dot/js/Range.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import { Node } from '../../../../scenery/js/imports.js';
+import PhetioObject, { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import BooleanIO from '../../../../tandem/js/types/BooleanIO.js';
 import IOType from '../../../../tandem/js/types/IOType.js';
 import NullableIO from '../../../../tandem/js/types/NullableIO.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
+import { CompositeSchema } from '../../../../tandem/js/types/StateSchema.js';
 import StringIO from '../../../../tandem/js/types/StringIO.js';
 import projectileMotion from '../../projectileMotion.js';
 import ProjectileMotionStrings from '../../ProjectileMotionStrings.js';
@@ -35,26 +38,58 @@ const tankShellString = ProjectileMotionStrings.tankShell;
 // constants
 const objectTypesTandem = Tandem.GLOBAL_MODEL.createTandem( 'projectileObjectTypes' );
 
+export type ProjectileObjectViewCreator = ( diameter: number, modelViewTransform: ModelViewTransform2, landed: boolean ) => Node;
+
+type SelfOptions = {
+  massRange?: Range;
+  massRound?: number;
+  diameterRange?: Range;
+  diameterRound?: number;
+
+  // most objectTypes have a max of 1
+  dragCoefficientRange?: Range;
+  viewCreationFunction?: ProjectileObjectViewCreator | null;
+};
+
+// viewCreationFunction should never be null if provided
+type ProjectileObjectTypeOptions = SelfOptions & PhetioObjectOptions & { viewCreationFunction?: ProjectileObjectViewCreator };
+
+type ProjectileObjectTypeStateObject = {
+  name: string | null;
+  mass: number;
+  diameter: number;
+  dragCoefficient: number;
+  benchmark: string | null;
+  rotates: boolean;
+  massRange: RangeStateObject;
+  massRound: number;
+  diameterRange: RangeStateObject;
+  diameterRound: number;
+  dragCoefficientRange: RangeStateObject;
+};
+
 class ProjectileObjectType extends PhetioObject {
+  public name: string | null; // name of the object, such as 'Golf ball', or null if it doesn't have a name
+  public benchmark: string | null; // identifier of the object benchmark, such as 'tankShell', also considered a 'name' for it like for Tandems. null for screens with only one object type
+  public rotates: boolean; // whether the object rotates or just translates in air
+  private mass: number; // in kg
+  private diameter: number; // in meters
+  private dragCoefficient: number;
+  public readonly initialMass: number;
+  public readonly initialDiameter: number;
+  public readonly initialDragCoefficient: number;
+  public massRange: Range;
+  public massRound: number;
+  public diameterRange: Range;
+  public diameterRound: number;
+  public dragCoefficientRange: Range;
+  public viewCreationFunction: ProjectileObjectViewCreator | null;
+  public readonly projectileObjectTypeOptions: ProjectileObjectTypeOptions;
 
-  /**
-   * @param {string|null} name - name of the object, such as 'Golf ball', or null if it doesn't have a name
-   * @param {number} mass - in kg
-   * @param {number} diameter - in meters
-   * @param {number} dragCoefficient
-   * @param {string|null} benchmark - identifier of the object benchmark, such as 'tankShell', also considered a
-   *                                      'name' for it like for Tandems. null for screens with only one object type
-   * @param {boolean} rotates - whether the object rotates or just translates in air
-   * @param {Object} [options]
-   */
-  constructor( name, mass, diameter, dragCoefficient, benchmark, rotates, options ) {
+  public constructor( name: string | null, mass: number, diameter: number, dragCoefficient: number,
+                      benchmark: string | null, rotates: boolean, providedOptions: ProjectileObjectTypeOptions ) {
 
-    // Options contains data about range and rounding for mass, diameter, drag coefficient.
-    // defaults to those of custom objects for screens that don't have benchmarks
-    options = merge( {
-      tandem: Tandem.REQUIRED,
-      phetioType: ProjectileObjectType.ProjectileObjectTypeIO,
-
+    const options = optionize<ProjectileObjectTypeOptions, SelfOptions, PhetioObjectOptions>()( {
       massRange: new Range( 1, 10 ),
       massRound: 1,
       diameterRange: new Range( 0.1, 1 ),
@@ -62,28 +97,29 @@ class ProjectileObjectType extends PhetioObject {
 
       // most objectTypes have a max of 1
       dragCoefficientRange: new Range( ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE.min, 1 ),
-      viewCreationFunction: null
-    }, options );
+      viewCreationFunction: null,
+      tandem: Tandem.REQUIRED,
+      phetioType: ProjectileObjectType.ProjectileObjectTypeIO
+    }, providedOptions );
 
     super( options );
 
-    // @public (read-only)
+    this.projectileObjectTypeOptions = options;
+
     this.name = name;
     this.benchmark = benchmark;
     this.rotates = rotates;
-    this.options = options; // stored to support `this.prototype.copy`
 
-    // @public - writable on the lab screen
+    // writable on the lab screen
     this.mass = mass;
     this.diameter = diameter;
     this.dragCoefficient = dragCoefficient;
 
-    // @private - these mutable values also store their initial values
+    // these mutable values also store their initial values
     this.initialMass = mass;
     this.initialDiameter = diameter;
     this.initialDragCoefficient = dragCoefficient;
 
-    // @public (read-only)
     this.massRange = options.massRange;
     this.massRound = options.massRound;
     this.diameterRange = options.diameterRange;
@@ -92,11 +128,7 @@ class ProjectileObjectType extends PhetioObject {
     this.viewCreationFunction = options.viewCreationFunction;
   }
 
-  /**
-   * @public
-   * @returns {Object}
-   */
-  toStateObject() {
+  public toStateObject(): ProjectileObjectTypeStateObject {
     return {
       name: NullableIO( StringIO ).toStateObject( this.name ),
       mass: this.mass,
@@ -112,12 +144,7 @@ class ProjectileObjectType extends PhetioObject {
     };
   }
 
-
-  /**
-   * @public
-   * @returns {Object}
-   */
-  static get STATE_SCHEMA() {
+  public static get STATE_SCHEMA(): CompositeSchema {
     return {
       name: NullableIO( StringIO ),
       mass: NumberIO,
@@ -133,11 +160,7 @@ class ProjectileObjectType extends PhetioObject {
     };
   }
 
-  /**
-   * @public
-   * @returns {Object}
-   */
-  applyState( stateObject ) {
+  private applyState( stateObject: ProjectileObjectTypeStateObject ): void {
     this.name = NullableIO( StringIO ).fromStateObject( stateObject.name );
     this.mass = stateObject.mass;
     this.diameter = stateObject.diameter;
@@ -150,198 +173,198 @@ class ProjectileObjectType extends PhetioObject {
     this.diameterRound = stateObject.diameterRound;
     this.dragCoefficientRange = Range.RangeIO.fromStateObject( stateObject.dragCoefficientRange );
   }
-}
 
-ProjectileObjectType.ProjectileObjectTypeIO = new IOType( 'ProjectileObjectTypeIO', {
-  valueType: ProjectileObjectType,
-  toStateObject: projectileObjectType => projectileObjectType.toStateObject(),
-  applyState: ( projectileObjectType, stateObject ) => projectileObjectType.applyState( stateObject ),
-  stateSchema: ProjectileObjectType.STATE_SCHEMA,
-  documentation: 'A data type that stores the variables for a given object type.'
-} );
-
-projectileMotion.register( 'ProjectileObjectType', ProjectileObjectType );
+  public static readonly ProjectileObjectTypeIO = new IOType( 'ProjectileObjectTypeIO', {
+    valueType: ProjectileObjectType,
+    toStateObject: projectileObjectType => projectileObjectType.toStateObject(),
+    applyState: ( projectileObjectType, stateObject ) => projectileObjectType.applyState( stateObject ),
+    stateSchema: ProjectileObjectType.STATE_SCHEMA,
+    documentation: 'A data type that stores the variables for a given object type.'
+  } );
 
 //-------------------------------------------------------------------------------------------
 // Specific projectile objects below ...
 //-------------------------------------------------------------------------------------------
 
-ProjectileObjectType.CANNONBALL = new ProjectileObjectType(
-  cannonballString,
-  ProjectileMotionConstants.CANNONBALL_MASS,
-  ProjectileMotionConstants.CANNONBALL_DIAMETER,
-  ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
-  'cannonball',
-  false, {
-    massRange: new Range( 1, 31.00 ),
-    massRound: 0.01,
-    diameterRange: new Range( 0.1, 1 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createCannonball,
-    tandem: objectTypesTandem.createTandem( 'cannonball' )
-  }
-);
+  public static readonly CANNONBALL = new ProjectileObjectType(
+    cannonballString,
+    ProjectileMotionConstants.CANNONBALL_MASS,
+    ProjectileMotionConstants.CANNONBALL_DIAMETER,
+    ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
+    'cannonball',
+    false, {
+      massRange: new Range( 1, 31.00 ),
+      massRound: 0.01,
+      diameterRange: new Range( 0.1, 1 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createCannonball,
+      tandem: objectTypesTandem.createTandem( 'cannonball' )
+    }
+  );
 
-ProjectileObjectType.PUMPKIN = new ProjectileObjectType(
-  pumpkinString,
-  5,
-  0.37,
-  0.6,
-  'pumpkin',
-  false, {
-    massRange: new Range( 1, 1000 ),
-    massRound: 1,
-    diameterRange: new Range( 0.1, 3 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createPumpkin,
-    tandem: objectTypesTandem.createTandem( 'pumpkin' )
-  }
-);
+  public static readonly PUMPKIN = new ProjectileObjectType(
+    pumpkinString,
+    5,
+    0.37,
+    0.6,
+    'pumpkin',
+    false, {
+      massRange: new Range( 1, 1000 ),
+      massRound: 1,
+      diameterRange: new Range( 0.1, 3 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createPumpkin,
+      tandem: objectTypesTandem.createTandem( 'pumpkin' )
+    }
+  );
 
-ProjectileObjectType.BASEBALL = new ProjectileObjectType(
-  baseballString,
-  0.15,
-  0.07,
-  0.35,
-  'baseball',
-  false, {
-    massRange: new Range( 0.01, 5 ),
-    massRound: 0.01,
-    diameterRange: new Range( 0.01, 1 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createBaseball,
-    tandem: objectTypesTandem.createTandem( 'baseball' )
-  }
-);
+  public static readonly BASEBALL = new ProjectileObjectType(
+    baseballString,
+    0.15,
+    0.07,
+    0.35,
+    'baseball',
+    false, {
+      massRange: new Range( 0.01, 5 ),
+      massRound: 0.01,
+      diameterRange: new Range( 0.01, 1 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createBaseball,
+      tandem: objectTypesTandem.createTandem( 'baseball' )
+    }
+  );
 
-ProjectileObjectType.CAR = new ProjectileObjectType(
-  carString,
-  2000,
-  2,
-  0.55,
-  'car',
-  true, {
-    massRange: new Range( 100, 5000 ),
-    massRound: 1,
-    diameterRange: new Range( 0.5, 3 ),
-    diameterRound: 0.1,
-    viewCreationFunction: ProjectileObjectViewFactory.createCar,
-    tandem: objectTypesTandem.createTandem( 'car' )
-  }
-);
+  public static readonly CAR = new ProjectileObjectType(
+    carString,
+    2000,
+    2,
+    0.55,
+    'car',
+    true, {
+      massRange: new Range( 100, 5000 ),
+      massRound: 1,
+      diameterRange: new Range( 0.5, 3 ),
+      diameterRound: 0.1,
+      viewCreationFunction: ProjectileObjectViewFactory.createCar,
+      tandem: objectTypesTandem.createTandem( 'car' )
+    }
+  );
 
-ProjectileObjectType.FOOTBALL = new ProjectileObjectType(
-  footballString,
-  0.41,
-  0.17,
-  0.05,
-  'football',
-  true, {
-    massRange: new Range( 0.01, 5 ),
-    massRound: 0.01,
-    diameterRange: new Range( 0.01, 1 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createFootball,
-    tandem: objectTypesTandem.createTandem( 'football' )
-  }
-);
+  public static readonly FOOTBALL = new ProjectileObjectType(
+    footballString,
+    0.41,
+    0.17,
+    0.05,
+    'football',
+    true, {
+      massRange: new Range( 0.01, 5 ),
+      massRound: 0.01,
+      diameterRange: new Range( 0.01, 1 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createFootball,
+      tandem: objectTypesTandem.createTandem( 'football' )
+    }
+  );
 
-ProjectileObjectType.HUMAN = new ProjectileObjectType(
-  humanString,
-  70,
-  0.5,
-  0.6,
-  'human',
-  true, {
-    massRange: new Range( 10, 200 ),
-    massRound: 1,
-    diameterRange: new Range( 0.1, 1.5 ),
-    diameterRound: 0.1,
-    viewCreationFunction: ProjectileObjectViewFactory.createHuman,
-    tandem: objectTypesTandem.createTandem( 'human' )
-  }
-);
+  public static readonly HUMAN = new ProjectileObjectType(
+    humanString,
+    70,
+    0.5,
+    0.6,
+    'human',
+    true, {
+      massRange: new Range( 10, 200 ),
+      massRound: 1,
+      diameterRange: new Range( 0.1, 1.5 ),
+      diameterRound: 0.1,
+      viewCreationFunction: ProjectileObjectViewFactory.createHuman,
+      tandem: objectTypesTandem.createTandem( 'human' )
+    }
+  );
 
-ProjectileObjectType.PIANO = new ProjectileObjectType(
-  pianoString,
-  400,
-  2.2,
-  ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE.max,
-  'piano',
-  false, {
-    massRange: new Range( 50, 1000 ),
-    massRound: 1,
-    diameterRange: new Range( 0.5, 3 ),
-    diameterRound: 0.1,
+  public static readonly PIANO = new ProjectileObjectType(
+    pianoString,
+    400,
+    2.2,
+    ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE.max,
+    'piano',
+    false, {
+      massRange: new Range( 50, 1000 ),
+      massRound: 1,
+      diameterRange: new Range( 0.5, 3 ),
+      diameterRound: 0.1,
 
-    // the piano can accept all drag coefficient ranges
-    dragCoefficientRange: ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE,
-    viewCreationFunction: ProjectileObjectViewFactory.createPiano,
-    tandem: objectTypesTandem.createTandem( 'piano' )
-  }
-);
+      // the piano can accept all drag coefficient ranges
+      dragCoefficientRange: ProjectileMotionConstants.PROJECTILE_DRAG_COEFFICIENT_RANGE,
+      viewCreationFunction: ProjectileObjectViewFactory.createPiano,
+      tandem: objectTypesTandem.createTandem( 'piano' )
+    }
+  );
 
-ProjectileObjectType.GOLF_BALL = new ProjectileObjectType(
-  golfBallString,
-  0.05,
-  0.04,
-  0.25,
-  'golfBall',
-  false, {
-    massRange: new Range( 0.01, 5 ),
-    massRound: 0.01,
-    diameterRange: new Range( 0.01, 1 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createGolfBall,
-    tandem: objectTypesTandem.createTandem( 'golfBall' )
-  }
-);
+  public static readonly GOLF_BALL = new ProjectileObjectType(
+    golfBallString,
+    0.05,
+    0.04,
+    0.25,
+    'golfBall',
+    false, {
+      massRange: new Range( 0.01, 5 ),
+      massRound: 0.01,
+      diameterRange: new Range( 0.01, 1 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createGolfBall,
+      tandem: objectTypesTandem.createTandem( 'golfBall' )
+    }
+  );
 
-ProjectileObjectType.TANK_SHELL = new ProjectileObjectType(
-  tankShellString,
-  42,
-  0.15,
-  0.06,
-  'tankShell',
-  true, {
-    massRange: new Range( 5, 200 ),
-    massRound: 1,
-    diameterRange: new Range( 0.1, 1 ),
-    diameterRound: 0.01,
-    viewCreationFunction: ProjectileObjectViewFactory.createTankShell,
-    tandem: objectTypesTandem.createTandem( 'tankShell' )
-  }
-);
+  public static readonly TANK_SHELL = new ProjectileObjectType(
+    tankShellString,
+    42,
+    0.15,
+    0.06,
+    'tankShell',
+    true, {
+      massRange: new Range( 5, 200 ),
+      massRound: 1,
+      diameterRange: new Range( 0.1, 1 ),
+      diameterRound: 0.01,
+      viewCreationFunction: ProjectileObjectViewFactory.createTankShell,
+      tandem: objectTypesTandem.createTandem( 'tankShell' )
+    }
+  );
 
-ProjectileObjectType.CUSTOM = new ProjectileObjectType(
-  customString,
-  100,
-  1,
-  ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
-  'custom',
-  true, {
-    massRange: new Range( 1, 5000 ),
-    massRound: 0.01,
-    diameterRange: new Range( 0.01, 3 ),
-    diameterRound: 0.01,
-    dragCoefficientRange: new Range( 0.04, 1 ),
-    tandem: objectTypesTandem.createTandem( 'custom' ),
-    phetioDocumentation: 'A custom projectile type that can have its values edited by the user'
-  }
-);
+  public static readonly CUSTOM = new ProjectileObjectType(
+    customString,
+    100,
+    1,
+    ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
+    'custom',
+    true, {
+      massRange: new Range( 1, 5000 ),
+      massRound: 0.01,
+      diameterRange: new Range( 0.01, 3 ),
+      diameterRound: 0.01,
+      dragCoefficientRange: new Range( 0.04, 1 ),
+      tandem: objectTypesTandem.createTandem( 'custom' ),
+      phetioDocumentation: 'A custom projectile type that can have its values edited by the user'
+    }
+  );
 
 // Meant to be used on screens that don't have object type selection, and only use a single object type
-ProjectileObjectType.COMPANIONLESS = new ProjectileObjectType(
-  null,
-  5,
-  0.8,
-  ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
-  null,
-  true, {
-    tandem: objectTypesTandem.createTandem( 'genericObjectType' ),
-    phetioDocumentation: 'On some screens there are only a single, general projectile object type. It cannot be ' +
-                         'changed to a different object type, but can be altered via Properties in the model.'
-  }
-);
+  public static readonly COMPANIONLESS = new ProjectileObjectType(
+    null,
+    5,
+    0.8,
+    ProjectileMotionConstants.CANNONBALL_DRAG_COEFFICIENT,
+    null,
+    true, {
+      tandem: objectTypesTandem.createTandem( 'genericObjectType' ),
+      phetioDocumentation: 'On some screens there are only a single, general projectile object type. It cannot be ' +
+                           'changed to a different object type, but can be altered via Properties in the model.'
+    }
+  );
+}
+
+projectileMotion.register( 'ProjectileObjectType', ProjectileObjectType );
 
 export default ProjectileObjectType;
