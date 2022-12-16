15fb5cf228a34946dfa89274f4c8010508f5cc66
Matthew Blackman <matthewblackman1@gmail.com>
2022-12-16 10:33:31 -0500

    Abstract projectile acceleration and gravityForce - see https://github.com/phetsims/projectile-motion/issues/308

--- js/common/model/Trajectory.ts
+++ js/common/model/Trajectory.ts
@@ -15,7 +15,6 @@ import createObservableArray, { ObservableArray } from '../../../../axon/js/crea
 import Emitter from '../../../../axon/js/Emitter.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Property from '../../../../axon/js/Property.js';
-import Utils from '../../../../dot/js/Utils.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import PhetioGroup from '../../../../tandem/js/PhetioGroup.js';
@@ -222,23 +221,19 @@ class Trajectory extends PhetioObject {
     this.updateTrajectoryRanksEmitter.addListener( incrementRank );
 
     // Set the initial velocity based on the initial speed and angle
-    const velocity = Vector2.pool
-      .fetch()
-      .setPolar( this.initialSpeed, ( this.initialAngle * Math.PI ) / 180 );
+    const velocity = Vector2.pool.fetch().setPolar( this.initialSpeed, ( this.initialAngle * Math.PI ) / 180 );
 
     const dragForce = this.dragForceForVelocity( velocity );
+    const acceleration = this.accelerationForDragForce( dragForce );
 
     const initialPoint = new DataPoint(
-      0, // total time elapsed
+      0,
       Vector2.pool.create( 0, this.initialHeight ), // position
       this.airDensityProperty.value,
       velocity,
-      Vector2.pool.create(
-        -dragForce.x / this.mass,
-        -this.gravityProperty.value - dragForce.y / this.mass
-      ), // acceleration
-      dragForce, // drag force
-      -this.gravityProperty.value * this.mass // force gravity
+      acceleration,
+      dragForce,
+      this.gravityForce()
     );
 
     this.addDataPoint( initialPoint );
@@ -275,6 +270,17 @@ class Trajectory extends PhetioObject {
     };
   }
 
+  private gravityForce(): number {
+    return -this.gravityProperty.value * this.mass;
+  }
+
+  /**
+   * @param dragForce - the drag force on the projectile
+   */
+  private accelerationForDragForce( dragForce: Vector2 ): Vector2 {
+    return Vector2.pool.fetch().setXY( -dragForce.x / this.mass, -this.gravityProperty.value - dragForce.y / this.mass );
+  }
+
   /**
    * @param velocity - the velocity of the projectile
    */
@@ -296,14 +302,14 @@ class Trajectory extends PhetioObject {
     const previousPoint = this.dataPoints.get( this.dataPoints.length - 1 );
 
     let newX = nextPosition( previousPoint.position.x, previousPoint.velocity.x, previousPoint.acceleration.x, dt );
-    const newY = nextPosition( previousPoint.position.y, previousPoint.velocity.y, previousPoint.acceleration.y, dt );
-
     let newVx = previousPoint.velocity.x + previousPoint.acceleration.x * dt;
+
+    const newY = nextPosition( previousPoint.position.y, previousPoint.velocity.y, previousPoint.acceleration.y, dt );
     const newVy = previousPoint.velocity.y + previousPoint.acceleration.y * dt;
 
     // if drag force reverses the x-velocity in this step, set vx to zero to better approximate reality
     // We do not need to do this adjustment for the y direction because gravity is already resulting in a change in
-    // direction, and because our air-resistance model cuts corners already (via linear interpolation).
+    // direction, and because our air-resistance model is not 100% accurate already (via linear interpolation).
     if ( Math.sign( newVx ) !== Math.sign( previousPoint.velocity.x ) ) {
       newVx = 0;
 
@@ -311,13 +317,14 @@ class Trajectory extends PhetioObject {
       newX = nextPosition( previousPoint.position.x, previousPoint.velocity.x, previousPoint.acceleration.x, newDt );
     }
 
-    const newPosition = Vector2.pool.create( newX, newY );
+    const newPosition = Vector2.pool.fetch().setXY( newX, newY );
     const newVelocity = Vector2.pool.fetch().setXY( newVx, newVy );
     const newDragForce = this.dragForceForVelocity( newVelocity );
+    const newAcceleration = this.accelerationForDragForce( newDragForce );
 
     //if the apex has been reached
     if ( previousPoint.velocity.y > 0 && newVelocity.y < 0 ) {
-      this.handleApex( previousPoint, newPosition, newVelocity, newDragForce, dt );
+      this.handleApex( previousPoint );
     }
 
     let newPoint;
@@ -329,12 +336,9 @@ class Trajectory extends PhetioObject {
         newPosition,
         this.airDensityProperty.value,
         newVelocity,
-        Vector2.pool.create(
-          -newDragForce.x / this.mass,
-          -this.gravityProperty.value - newDragForce.y / this.mass
-        ), // acceleration
+        newAcceleration,
         newDragForce,
-        -this.gravityProperty.value * this.mass
+        this.gravityForce()
       );
     }
     else { // Has reached ground or below
@@ -365,29 +369,24 @@ class Trajectory extends PhetioObject {
           previousPoint.acceleration.y;
       }
 
-      newX =
-        previousPoint.position.x +
-        previousPoint.velocity.x * timeToGround +
-        0.5 * previousPoint.acceleration.x * timeToGround * timeToGround;
+      newX = nextPosition( previousPoint.position.x, previousPoint.velocity.x, previousPoint.acceleration.x, timeToGround );
 
-      const newPosition = Vector2.pool.create( newX, 0 );
+      const newPosition = Vector2.pool.fetch().setXY( newX, 0 );
 
       const impactVx = previousPoint.velocity.x + previousPoint.acceleration.x * timeToGround;
       const impactVy = previousPoint.velocity.y + previousPoint.acceleration.y * timeToGround;
       const impactVelocity = Vector2.pool.fetch().setXY( impactVx, impactVy );
       const impactDragForce = this.dragForceForVelocity( impactVelocity );
+      const impactAcceleration = this.accelerationForDragForce( impactDragForce );
 
       newPoint = new DataPoint(
         previousPoint.time + timeToGround,
         newPosition,
         this.airDensityProperty.value,
         impactVelocity,
-        Vector2.pool.create(
-          -impactDragForce.x / this.mass,
-          -this.gravityProperty.value - impactDragForce.y / this.mass
-        ), // acceleration
-        impactDragForce, // drag force
-        -this.gravityProperty.value * this.mass, // force gravity
+        impactAcceleration,
+        impactDragForce,
+        this.gravityForce(),
         {
           // add this special property to just the last datapoint collected for a trajectory
           reachedGround: true
@@ -419,31 +418,28 @@ class Trajectory extends PhetioObject {
    * @param newDragForce
    * @param dt
    */
-  private handleApex( previousPoint: DataPoint, newPosition: Vector2, newVelocity: Vector2, newDragForce: Vector2, dt: number ): void {
+  private handleApex( previousPoint: DataPoint ): void {
     // These are all approximations if there is air resistance
-    const dtToApex = Utils.linear( previousPoint.velocity.y, newVelocity.y, 0, dt, 0 );
-    const apexX = Utils.linear( 0, dt, previousPoint.position.x, newPosition.x, dtToApex );
-    const apexY =
-      previousPoint.position.y +
-      previousPoint.velocity.y * dtToApex +
-      0.5 * previousPoint.acceleration.y * dtToApex * dtToApex;
+    const dtToApex = Math.abs( previousPoint.velocity.y / previousPoint.acceleration.y );
+    const apexX = nextPosition( previousPoint.position.x, previousPoint.velocity.x, previousPoint.acceleration.x, dtToApex );
+    const apexY = nextPosition( previousPoint.position.y, previousPoint.velocity.y, previousPoint.acceleration.y, dtToApex );
 
-    const apexVelocityX = Utils.linear( 0, dt, previousPoint.velocity.x, newVelocity.x, dtToApex );
-    const apexVelocityY = Utils.linear( 0, dt, previousPoint.velocity.y, newVelocity.y, dtToApex );
-    const apexDragX = Utils.linear( 0, dt, previousPoint.dragForce.x, newDragForce.x, dtToApex );
-    const apexDragY = Utils.linear( 0, dt, previousPoint.dragForce.y, newDragForce.y, dtToApex );
+    const apexVelocityX = previousPoint.velocity.x + previousPoint.acceleration.x * dtToApex;
+    const apexVelocityY = 0; // by definition this is what makes it the apex
+    const apexVelocity = Vector2.pool.fetch().setXY( apexVelocityX, apexVelocityY );
+
+    const apexDragForce = this.dragForceForVelocity( apexVelocity );
+    const apexAcceleration = this.accelerationForDragForce( apexDragForce );
 
     const apexPoint = new DataPoint(
       previousPoint.time + dtToApex,
-      Vector2.pool.create( apexX, apexY ),
+      Vector2.pool.fetch().setXY( apexX, apexY ),
       this.airDensityProperty.value,
-      Vector2.pool.create( apexVelocityX, apexVelocityY ), // velocity
-      Vector2.pool.create(
-        -apexDragX / this.mass,
-        -this.gravityProperty.value - apexDragY / this.mass
-      ), // acceleration
-      Vector2.pool.create( apexDragX, apexDragY ), // drag force
-      -this.gravityProperty.value * this.mass, {
+      apexVelocity,
+      apexAcceleration,
+      apexDragForce,
+      this.gravityForce(),
+      {
         apex: true
       }
     );
