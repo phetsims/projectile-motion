72b5a28c301b465d23097a2a9e1b778bdd7a4369
Matthew Blackman <matthewblackman1@gmail.com>
2022-12-16 20:54:51 -0500

    Convert FireButton.ts - see https://github.com/phetsims/projectile-motion/issues/306

--- js/common/view/FireButton.ts
+++ js/common/view/FireButton.ts
@@ -6,23 +6,25 @@
  * @author Andrea Lin (PhET Interactive Simulations)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import { Image } from '../../../../scenery/js/imports.js';
-import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
+import RectangularPushButton, { RectangularPushButtonOptions } from '../../../../sun/js/buttons/RectangularPushButton.js';
 import fireButton_png from '../../../mipmaps/fireButton_png.js';
 import projectileMotion from '../../projectileMotion.js';
 
+type SelfOptions = {
+  baseColor:string;
+  iconWidth?: number;
+};
+type FireButtonOptions = SelfOptions & RectangularPushButtonOptions;
+
 class FireButton extends RectangularPushButton {
+  public constructor( providedOptions: FireButtonOptions ) {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
-
-    options = merge( {
+    const options = optionize<FireButtonOptions, SelfOptions, RectangularPushButtonOptions>()( {
       baseColor: 'rgb( 234,33,38 )', // cannon red
       iconWidth: 20 // width of icon, used for scaling, the aspect ratio will determine height
-    }, options );
+    }, providedOptions );
 
     // fire button icon
     assert && assert( !options.content, 'this type sets its own content' );
