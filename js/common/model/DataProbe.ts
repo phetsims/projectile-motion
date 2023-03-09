b3aa9bf6a3270c33ce08b6efa89c6dfc0deea5cc
Matthew Blackman <matthewblackman1@gmail.com>
2023-03-09 09:23:14 -0500

    Fix more awkward formatting - see https://github.com/phetsims/projectile-motion/issues/315

--- js/common/model/DataProbe.ts
+++ js/common/model/DataProbe.ts
@@ -99,8 +99,8 @@ class DataProbe {
         return;
       }
       const point = currentTrajectory.getNearestPoint( this.positionProperty.get().x, this.positionProperty.get().y );
-      const pointIsReadable = point && ( point.apex || point.position.y === 0 ||
-                                         Utils.toFixedNumber( point.time * 1000, 0 ) % TIME_PER_MINOR_DOT === 0 );
+      const pointIsReadable = point &&
+                              ( point.apex || point.position.y === 0 || Utils.toFixedNumber( point.time * 1000, 0 ) % TIME_PER_MINOR_DOT === 0 );
       if ( pointIsReadable && this.pointWithinTolerance( point.position ) ) {
         this.dataPointProperty.set( point );
         return;
@@ -114,8 +114,8 @@ class DataProbe {
    */
   public updateDataIfWithinRange( point: DataPoint ): void {
     // point can be read by dataProbe if it exists, it is on the ground, or it is the right timestep
-    const pointIsReadable = point && ( point.apex || point.position.y === 0 ||
-                              Utils.toFixedNumber( point.time * 1000, 0 ) % TIME_PER_MINOR_DOT === 0 );
+    const pointIsReadable = point &&
+                            ( point.apex || point.position.y === 0 || Utils.toFixedNumber( point.time * 1000, 0 ) % TIME_PER_MINOR_DOT === 0 );
     if ( pointIsReadable && this.pointWithinTolerance( point.position ) ) {
       this.dataPointProperty.set( point );
     }
