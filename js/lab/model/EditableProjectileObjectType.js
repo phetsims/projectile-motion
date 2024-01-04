f8c5e6d3c7c71713454d4fdcda9f0b4d9d23aea9
phet-dev <phetadmin@gmail.com>
2024-01-04 03:19:48 -0700

    update copyright dates from daily grunt work

--- js/lab/model/EditableProjectileObjectType.js
+++ js/lab/model/EditableProjectileObjectType.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * ProjectileObjectType subtype that contains editable (mutable) properties of the projectile type
