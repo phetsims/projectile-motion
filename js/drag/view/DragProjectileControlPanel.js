a960824663dee7b1584b4c2d5a5a4c7d8082ad4e
phet-dev <phetadmin@gmail.com>
2024-03-23 03:25:30 -0600

    update copyright dates from daily grunt work

--- js/drag/view/DragProjectileControlPanel.js
+++ js/drag/view/DragProjectileControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /**
  * Control panel that allows users to alter properties of the projectile fired.
