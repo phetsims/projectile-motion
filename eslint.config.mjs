ac7787a976633426b39d4290c4cbc2b3824915a4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:47 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for projectile-motion.
+ * ESLint configuration for projectile-motion.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
