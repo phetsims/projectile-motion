77161469f3d32f47f0450e5148cc5a0c7b295183
matthew-blackman <mabl4992@colorado.edu>
2025-03-19 12:32:54 -0400

    Use Property<Vector2> for measuring tape base/tip position - see https://github.com/phetsims/projectile-motion/issues/306

--- js/common/model/ProjectileMotionMeasuringTape.ts
+++ js/common/model/ProjectileMotionMeasuringTape.ts
@@ -10,26 +10,27 @@
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import Vector2Property from '../../../../dot/js/Vector2Property.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import projectileMotion from '../../projectileMotion.js';
 
 class ProjectileMotionMeasuringTape {
 
-  public readonly basePositionProperty: Vector2Property;
-  public readonly tipPositionProperty: Vector2Property;
+  public readonly basePositionProperty: Property<Vector2>;
+  public readonly tipPositionProperty: Property<Vector2>;
   public readonly isActiveProperty: Property<boolean>;
 
   public constructor( tandem: Tandem ) {
-    this.basePositionProperty = new Vector2Property( new Vector2( 0, 0 ), {
+    this.basePositionProperty = new Property( new Vector2( 0, 0 ), {
       tandem: tandem.createTandem( 'basePositionProperty' ),
       units: 'm',
+      phetioValueType: Vector2.Vector2IO,
       phetioDocumentation: 'Base (start of tape from the container) position'
     } );
 
-    this.tipPositionProperty = new Vector2Property( new Vector2( 1, 0 ), {
+    this.tipPositionProperty = new Property( new Vector2( 1, 0 ), {
       tandem: tandem.createTandem( 'tipPositionProperty' ),
       units: 'm',
+      phetioValueType: Vector2.Vector2IO,
       phetioDocumentation: 'Tip (end of measuring tape) position'
     } );
 
