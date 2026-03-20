fae77dcbda10681dab5642a90285740faa4ca2fb
phet-dev <phetadmin@gmail.com>
2026-03-20 11:11:31 -0600

    update copyright dates from daily grunt work

--- js/common/view/ArrowlessNumberControl.ts
+++ js/common/view/ArrowlessNumberControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2025, University of Colorado Boulder
+// Copyright 2019-2026, University of Colorado Boulder
 
 /**
  * A specific NumberControl for this sim that doesn't have tweaker buttons.
--- js/lab/view/InitialValuesPanel.ts
+++ js/lab/view/InitialValuesPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2025, University of Colorado Boulder
+// Copyright 2017-2026, University of Colorado Boulder
 
 /**
  * Control panel that allows users to modify initial values for how the cannon fires a projectile.
